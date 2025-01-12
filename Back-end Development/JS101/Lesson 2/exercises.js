8b63bdfa0aa6d605c07c8f6106fe764b7a6e364e
Austin Kim <austinkim61@gmail.com>
2025-01-12 17:10:39 -0500

    Updated JS101 Notes

---
+++ Back-end Development/JS101/Lesson 2/exercises.js
@@ -0,0 +1,23 @@
+/*
+
+// Given a sentence made up of several words,
+// write a method to return the longest word.
+// Words consist of any text separated by a
+// single space.
+
+Split the sentence into words
+Save the first word as the starting word.
+For each of the remaining words in the sentence:
+  If the current word is longer than the saved word:
+    Reassign the saved word as the new current word.
+Return the saved word.
+
+
+
+
+
+
+
+
+
+*/
