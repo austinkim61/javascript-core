a4e1f5157816b600775b304333c2f59ec4ae8b63
Austin Kim <austinkim61@gmail.com>
2025-01-13 18:45:21 -0500

    Added practice problems

---
+++ back-end-development/js101/lesson-2/practice-problems-easy-1.js
@@ -0,0 +1,30 @@
+// let ages = { Herman: 32, Lily: 30, Grandpa: 5843, Eddie: 10 };
+
+// let additionalAges = { Marilyn: 22, Spot: 237 };
+
+// // ages[Object.keys(additionalAges)[0]] = Object.values(additionalAges)[0];
+// // ages[Object.keys(additionalAges)[1]] = Object.values(additionalAges)[1];
+
+// // console.log(ages);
+
+// let newAges = Object.assign(ages, additionalAges)
+
+// console.log(newAges);
+
+
+// let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
+
+// flintstones[flintstones.length] = "Dino";
+
+// console.log(flintstones);
+
+
+
+// let advice = "Few things in life are as important as house training your pet dinosaur.";
+
+// console.log(advice.substring(0, advice.indexOf('house')));
+
+// console.log(advice.slice(0, advice.indexOf('house')));
+
+
+
