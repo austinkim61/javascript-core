5f58bb40e93d3af6cf1c7acc19ac9759b368bcf1
Austin Kim <austinkim61@gmail.com>
2025-01-14 22:02:54 -0500

    Finished practice problems easy 3

---
+++ back-end-development/js101/lesson-3/practice-problems-easy-3.js
@@ -0,0 +1,174 @@
+// Question # 1
+// let numbers = [1, 2, 3, 4];
+
+// numbers.shift();
+// numbers.shift();
+// numbers.shift();
+// numbers.shift();
+// console.log(numbers);
+
+// numbers.pop();
+// numbers.pop();
+// numbers.pop();
+// numbers.pop();
+// console.log(numbers);
+
+// numbers.splice(0, numbers.length);
+// console.log(numbers);
+
+// numbers.length = 0;
+// console.log(numbers);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Question # 2
+// console.log([1, 2, 3] + [4, 5]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Question # 3
+// let str1 = "hello there";
+// let str2 = str1;
+// str2 = "goodbye!";
+// console.log(str1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Question # 4
+// let arr1 = [{ first: "value1" }, { second: "value2" }, 3, 4, 5];
+// let arr2 = arr1.slice();
+// arr2[0].first = 42;
+// arr2[2] = 4535;
+// console.log(arr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Question # 5
+// function isColorValid(color) {
+//   if (color === "blue" || color === "green") {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+
+// function isColorValid(color) {
+//   let colors = ["blue", "green"];
+//   return colors.includes(color);
+// }
+// console.log(isColorValid("blue"));
+
+
+
+
+
+
+// function isColorValid(color) {
+//   return (color.includes("blue") || color.includes("green"));
+// }
+// console.log(isColorValid("green"));
+
+
+
+
+
+
+
+
+// Question # 6
+// let arr = [1, 2, 3];
+// let newArr = arr;
+
+// const num = arr[0];
+// let newNum = num;
+
+// function double(num) {
+//   return num * 2;
+// }
+
+// double(newNum);
+
+/* Variables
+arr
+newArr
+num
+newNum
+double
+num
+*/
+
+
+
+/* Primitives
+line 1 - 1 2 3
+line 4 - 1 0
+line 5 - 1
+line 8 - 2
+line 11 - 1 2
+*/
+
+
+
+/* Objects
+[1, 2, 3]
+double function
+*/
+
+
+
+
+
