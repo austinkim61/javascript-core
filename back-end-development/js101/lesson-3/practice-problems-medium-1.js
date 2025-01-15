25db6731dfa3889dc23ccc0e9a11a576441b8d57
Austin Kim <austinkim61@gmail.com>
2025-01-15 17:13:31 -0500

    Updated practice problems

--- back-end-development/js101/lesson-3/practice-problems-medium-1.js
+++ back-end-development/js101/lesson-3/practice-problems-medium-1.js
@@ -29,88 +29,33 @@ loop foreach if letter.toUpperCase === letter, turn to lowercase and opposite fo
 */
 
 let munstersDescription = "The Munsters are creepy and spooky.";
-let split = munstersDescription.split(' ');
-// let nested = [...split[0]];
-// [t, h, e]
-// // this code below works
-// nested.forEach((element, index) => {
-  // if (element === element.toUpperCase()) {
-  //   nested[index] = element.toLowerCase();
-  // } else {
-  //   nested[index] = element.toUpperCase();
+// let split = munstersDescription.split('');
+// split.forEach((element, index) => {
+//   if (element === element.toUpperCase()) {
+//     split[index] = element.toLowerCase();
+//   } else {
+//     split[index] = element.toUpperCase();
 //   }
 // });
-// console.log(nested);
+// console.log(split.join(''));
 
 
-
-
-
-
-console.log(split);
-
-
-// split = [ 'The', 'Munsters', 'are', 'creepy', 'and', 'spooky.' ]
-
-// console.log(word[i]);
-
-
-split.forEach((word, index) => {
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] === word[i].toUpperCase()) {
-      split[index][i] = word[i].toLowerCase();
+console.log(munstersDescription
+  .split("")
+  .map(function (char) {
+    if (char === char.toUpperCase()) {
+      return char.toLowerCase();
     } else {
-      split[index][i] = word[i].toUpperCase();
+      return char.toUpperCase();
     }
-  }
-})
-
-
-console.log(split);
-
-
-
-// for (let i = 0; i < split.length; i++) {
-//   for (let j = 0; j < split[i].length; j++) {
-//     if (split[i][j] === split[i][j].toUpperCase()) {
-//       split[i][j] = split[i][j].toLowerCase();
-//     } else {
-//       split[i][j] = split[i][j].toUpperCase();
-//     }
-//   }
-// }
+  })
+  .join("")
+);
 
 
 
 
 
-
-
-
-
-
-
-
-
-// split[0][0] = split[0][0].toLowerCase();
-
-// console.log(split);
-
-// for (let i = 0; i < split.length; i++) {
-//   for (let j = 0; j < split[i].length; j++) {
-//     if (split[i][j] === split[i][j].toUpperCase()) {
-//       split[i][j] = split[i][j].toLowerCase();
-//     } else {
-//       split[i][j] = split[i][j].toUpperCase();
-//     }
-//   }
-// }
-
-// console.log(split[0][1].toUpperCase());
-
-
-// console.log(munstersDescription.split(' '));
-// console.log(munstersDescription.split(' ').join(' '));
 
 
 /* Question 3
