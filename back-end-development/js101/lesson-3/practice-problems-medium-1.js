fa016c3bbee89fcaaec8c607f7a9d9c746b0ec4d
Austin Kim <austinkim61@gmail.com>
2025-01-15 20:04:51 -0500

    Added exercises

--- back-end-development/js101/lesson-3/practice-problems-medium-1.js
+++ back-end-development/js101/lesson-3/practice-problems-medium-1.js
@@ -1,5 +1,5 @@
-/* Question 1
-*/
+// Question 1
+
 
 // let string = "The Flintstones Rock!";
 
@@ -16,19 +16,10 @@
 
 
 
+// Question 2
 
 
-
-/* Question 2
-
-split to array
-loop foreach if letter.toUpperCase === letter, turn to lowercase and opposite for lowercase
-
-
-
-*/
-
-let munstersDescription = "The Munsters are creepy and spooky.";
+// let munstersDescription = "The Munsters are creepy and spooky.";
 // let split = munstersDescription.split('');
 // split.forEach((element, index) => {
 //   if (element === element.toUpperCase()) {
@@ -39,59 +30,103 @@ let munstersDescription = "The Munsters are creepy and spooky.";
 // });
 // console.log(split.join(''));
 
-
-console.log(munstersDescription
-  .split("")
-  .map(function (char) {
-    if (char === char.toUpperCase()) {
-      return char.toLowerCase();
-    } else {
-      return char.toUpperCase();
-    }
-  })
-  .join("")
-);
-
+// console.log(munstersDescription
+//   .split("")
+//   .map(function (char) {
+//     if (char === char.toUpperCase()) {
+//       return char.toLowerCase();
+//     } else {
+//       return char.toUpperCase();
+//     }
+//   })
+//   .join("")
+// );
 
 
 
 
 
-
-/* Question 3
-
+// Question 3
 
 
 
+// function factors(number) {
+//   let divisor = number;
+//   let factors = [];
+
+//   if (isNaN(number)) {
+//     return "Not a number.";
+//   } else if (number === 0) {
+//     return "Number is zero.";
+//   } else if (!Number.isFinite(number)) {
+//     return "Number is infinite.";
+//   } else if (!Number.isInteger(number)) {
+//     return "Number is not an integer.";
+//   } else if (number < 0) {
+//     return "Number is negative.";
+//   } else {
+//     while (divisor !== 0) {
+//       if (number % divisor === 0) {
+//         factors.push(number / divisor);
+//       }
+//       divisor--;
+//     }
+//     return factors;
+//   }
+// }
+// console.log(factors(720));
 
 
 
+// function factors(number) {
+//   let divisor = number;
+//   let factors = [];
+//   while (divisor > 0) {
+//     if (number % divisor === 0) {
+//       factors.push(number / divisor);
+//     }
+//     divisor -= 1;
+//   }
+//   return factors;
+// }
 
-*/
+
+
 
 
 /* Question 4
+function addToRollingBuffer1(buffer, maxBufferSize, newElement) {     // buffer is an array // newElement is a primitive // maxbuffersize is the max array length
+  buffer.push(newElement);     // pushes the new primitive everytime the function is called. this mutates the array
+  if (buffer.length > maxBufferSize) {     // after pushing, checks to see if the new array length is greater than the max buffer size
+    buffer.shift();     // if the new array length is greater than the max buffer size, removes the first element of the array. shift method is mutating
+  }
+  return buffer; // returns the array
+}
 
 
 
-
-
-
-
-
+function addToRollingBuffer2(buffer, maxBufferSize, newElement) {     // buffer is an array // newElement is a primitive // maxbuffersize is the max array length
+  buffer = buffer.concat(newElement);     // merges the original array with the new primitive at the end but treats it as an array. assigns this value to buffer
+  if (buffer.length > maxBufferSize) {     // after reassigning buffer, checks to see if the new array length is greater than the max buffer size
+    buffer.shift();     // if the new array length is greater than the max buffer size, removes the first element of the array. shift method is mutating
+  }
+  return buffer; // returns the array // any additional invocation of this function will call the original array as that was not mutated
+}
 */
 
 
-/* Question 5
 
 
 
+// Question 5
 
 
 
+console.log(0.3 + 0.6); // 0.9
+console.log(0.3 + 0.6 === 0.9); // true
 
 
-*/
+
 
 
 /* Question 6
