a6adc17acf1d6f2e72ff9de96f97d8246e3b5f38
Austin Kim <austinkim61@gmail.com>
2025-01-19 02:15:56 -0500

    Preparing for test. Updated notes

--- back-end-development/js109/exercises.js
+++ back-end-development/js109/exercises.js
@@ -1,32 +1,115 @@
-let string1 = 'abcdefg';
-let string2 = 'This is a sentence.';
-let string3 = '12345';
-let string4 = '';
-let array1 = ['item1', 'item2', 'item3', 'item4', 'item5'];
-let array2 = [1, 2, 3, 4, 5];
-let object1 = {
-  name: 'Austin',
-  age: 37,
-  hobby: 'coding'
-};
+// let string1 = 'abcdefg';
+// let string2 = 'This is a sentence.';
+// let string3 = '12345';
+// let string4 = '';
+// let array1 = ['item1', 'item2', 'item3', 'item4', 'item5'];
+// let array2 = [1, 2, 3, 4, 5];
+// let object1 = {
+//   name: 'Austin',
+//   age: 37,
+//   hobby: 'coding'
+// };
+
+// let a1 = string4.split('a');
+// let a2 = string2.split('', 9);
+// let a3 = string4.split();
+// let a4 = string4.split('');
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);
+// console.log(a4);
+
+
+// let array1 = [1, 2, 3, 4, 5];
+// console.log(array1);
+// let array3 = array1.filter((number) => number >= 3);
+// console.log(array3);
+// array3[0] = 10;
+// console.log(array1);
+// console.log(array3);
+
+// let array2 = [[1], [2], [3,], [4], [5]];
+// let array4 = array2.filter((number) => number);
+// console.log(array2);
+// console.log(array4);
+
+// array4[0].push(100);
+
+
+// console.log(array2);
+// console.log(array4);
+
+
+// const arr1 = [0, 1, [2, [3, [4, 5]]]];
+
+// console.log(arr1.flat());
+// console.log(arr1.flat(1));
+// console.log(arr1.flat(2));
+// console.log(arr1.flat(3));
+// console.log(arr1.flat(4));
+// console.log(arr1.flat(Infinity));
+
+// let array1 = [1, 2, 3, 4, 5];
+// console.log(array1.join());
+// console.log(array1.join(', '));
+// console.log(array1.join(''));
+// console.log(array1.join(' '));
+// console.log(array1.join('.'));
 
 
 
-let a1 = string4.split('a');
-let a2 = string2.split('', 9);
-let a3 = string4.split();
-let a4 = string4.split('');
+// const plants = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+// let array1 = plants;
+// console.log(plants);
+// console.log(array1);
+// console.log(array1.pop());
+
+// console.log(plants);
+// console.log(array1);
 
 
-console.log(a1);
-console.log(a2);
-console.log(a3);
-console.log(a4);
-// console.log();
-// console.log();
-// console.log();
-// console.log();
 
+// const stringsArray = ["Hello", "world", "this", "is", "JavaScript"];
+
+
+// let newArray = stringsArray.reduce((accumulator, currentValue) => {
+//   accumulator.push(currentValue + "2");
+//   return accumulator;
+// }, []); // Initial value is an empty string
+
+// console.log(newArray);
+
+// let array = ["Hello", "world", "this", "is", "JavaScript"];
+// console.log(array);
+// array.reverse();
+// console.log(array);
+
+
+
+
+// const mixedNumericArray = ["80", "9", "700", 40, 1, 5, 200];
+// mixedNumericArray.sort((a, b) => b - a);
+// console.log(mixedNumericArray);
+
+
+
+// const matrix = [
+//   [1, 2, 3, [4, 5, 6, [4, 5, 6, [4, 5, 6], [4, 5, 6]]]],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
+
+// console.log(matrix.toString());
+
+let arr = [4, 5, 6];
+let arr2 = arr;
+console.log(arr);
+console.log(arr2);
+
+
+arr.unshift(1, 2, 3);
+console.log(arr);
+console.log(arr2);
 
 
 
