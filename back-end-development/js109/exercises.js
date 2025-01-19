55a1b1aa6f58eb402db2947e7a906e5a6059d821
Austin Kim <austinkim61@gmail.com>
2025-01-18 23:35:23 -0500

    Updated notes

---
+++ back-end-development/js109/exercises.js
@@ -0,0 +1,35 @@
+let string1 = 'abcdefg';
+let string2 = 'This is a sentence.';
+let string3 = '12345';
+let string4 = '';
+let array1 = ['item1', 'item2', 'item3', 'item4', 'item5'];
+let array2 = [1, 2, 3, 4, 5];
+let object1 = {
+  name: 'Austin',
+  age: 37,
+  hobby: 'coding'
+};
+
+
+
+let a1 = string4.split('a');
+let a2 = string2.split('', 9);
+let a3 = string4.split();
+let a4 = string4.split('');
+
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+// console.log();
+// console.log();
+// console.log();
+// console.log();
+
+
+
+
+
+
+
