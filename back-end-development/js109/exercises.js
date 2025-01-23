1a284efe98645f22d4048280d2ae40fc40f9d7e9
Austin Kim <austinkim61@gmail.com>
2025-01-23 00:19:48 -0500

    Removed files

--- back-end-development/js109/exercises.js
+++ back-end-development/js109/exercises.js
@@ -132,14 +132,14 @@
 
 
 
-let obj = { 1: 'a', 2: 'b', 3: 'c' };
-let array = Object.keys(obj);
+// let obj = { 1: 'a', 2: 'b', 3: 'c' };
+// let array = Object.keys(obj);
 
-let start = 0;
+// let start = 0;
 
-let sum = array.reduce(
-  (acc, string) => acc + Number(string),
-  start,
-);
+// let sum = array.reduce(
+//   (acc, string) => acc + Number(string),
+//   start,
+// );
 
-console.log(sum); 
+// console.log(sum); 
