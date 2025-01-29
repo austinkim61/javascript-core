1d269f4e29539a3bf6f3524940217ad94c992ecc
Austin Kim <austinkim61@gmail.com>
2025-01-29 18:17:08 -0500

    moved around files and updated

--- back-end-development/js109/exercises.js
+++ back-end-development/js109/exercises.js
@@ -9,3 +9,47 @@
 // );
 
 // console.log(sum); 
+
+
+// let arr1 = [[1], [2], [3]];
+// let arr2 = arr1.concat();
+
+
+// // arr1[0] = [2];
+// arr1[0].push('a');
+
+// console.log(arr1);
+// console.log(arr2);
+
+
+
+// let arr1 = [[1], [2], [3]];
+// let arr2 = arr1.map((x) => x);
+
+// console.log(arr1);
+// console.log(arr2);
+
+// arr1[0].push('a');
+
+// console.log(arr1);
+// console.log(arr2);
+
+
+// // function
+// function greet(name, param) {
+//   console.log('Hi' + ' ' + name);
+//   param();
+// }
+
+// // callback function
+// function callMe() {
+//   console.log('I am callback function');
+// }
+
+// // passing function as an argument
+// greet('Peter', callMe);
+
+
+
+
+
