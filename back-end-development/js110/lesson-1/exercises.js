8368290f10f8b30f14c21ded1f3ff3f6d33bfce8
Austin Kim <austinkim61@gmail.com>
2025-01-31 22:39:45 -0500

    updated notes and tracking

--- back-end-development/js110/lesson-1/exercises.js
+++ back-end-development/js110/lesson-1/exercises.js
@@ -1,6 +1,56 @@
+// let obj = { fruit: 'apple', vegetable: 'carrot' };
+// console.log(obj.fruit);
+// console.log(obj.fruit[3]);
+// console.log(obj['vegetable']);
+// console.log(obj['vegetable'][0]);
+
+// console.log(obj[0]);
+
+
+
+// let arr = ['foo', 'bar', 'qux'];
+// arr[3] = 5;
+// arr[10] = 52;
+// arr['boo'] = 'hoo';
+// arr[-1] = 374;
+
+// console.log(arr);
+// console.log(arr.length);
+// arr.forEach(element => console.log(element));
+// console.log(Object.keys(arr));
+
+
+
+// let arr = ['foo', 'bar', 'qux'];
+// arr[0] = 374;
+// console.log(arr);
+
+
+// let arr = ['a', 'b', 'c', 'd', 'e', 'f'];
+// console.log(arr.join(''));
+// console.log(arr.join(' '));
+
+
+// let numbers = [1, 2, 3, 4];
+// numbers[0] = numbers[0] + 1;
+// numbers[1] = numbers[1] + 1;
+// numbers[2] = numbers[2] + 1;
+// numbers[3] = numbers[3] + 1;
+// numbers[4] = numbers[4] + 1;
+
+
+// console.log(numbers);
 
 
 
 
+let obj = { apple: 'Produce', carrot: 'Produce', pear: 'Produce', broccoli: 'Produce' };
+obj['apple'] = 'Fruit';
+obj.carrot = 'Vegetable';
+obj.pear = 'Fruit';
 
+let key = 'broccoli';
+obj[key] = 'Vegetable';
+
+console.log(obj);
 
