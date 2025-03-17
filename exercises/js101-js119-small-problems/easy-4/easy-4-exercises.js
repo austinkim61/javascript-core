3a9e098cedd952c54dcd580307451ee303de2d49
Austin Kim <austinkim61@gmail.com>
2025-03-16 21:23:23 -0400

    more exercises

--- exercises/js101-js119-small-problems/easy-4/easy-4-exercises.js
+++ exercises/js101-js119-small-problems/easy-4/easy-4-exercises.js
@@ -356,21 +356,55 @@
 
 
 // 10 - Convert a String to a Signed Number
+function stringToSignedInteger(string) {  
+  let sign = signed(string);
+  let value = stringToInteger(string);
+
+  let multiplier = sign + 1;
+  value = multiplier * value;
+  console.log(value);
+  return value;
+
+}
 
 
 
 
 
+function stringToInteger(string) {
+  const DIGITS = {
+    0: 0,
+    1: 1,
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9
+  };
+  let value = 0;
+  let arrayOfDigits = string.replace(/[+-]/, '').split("").map(char => DIGITS[char]);  
+  arrayOfDigits.forEach(digit => (value = (10 * value) + digit));
+  return value;
+}
+
+function signed(string) {
+  if (string[0] === ('-' || '+')) {
+    let symbol = string[0];
+    return symbol;
+  }
+  return '';
+}
 
 
 
 
 
-
-
-
-
-
+console.log(stringToSignedInteger("4321") === 4321); // logs true
+console.log(stringToSignedInteger("-570") === -570); // logs true
+console.log(stringToSignedInteger("+100") === 100); // logs true
 
 
 
