ffb1a5c6b99561febd4f5057b790e73ad7f69dda
Austin Kim <austinkim61@gmail.com>
2025-03-17 23:39:54 -0400

    more exercises

--- exercises/js101-js119-small-problems/easy-5/easy-5-exercises.js
+++ exercises/js101-js119-small-problems/easy-5/easy-5-exercises.js
@@ -118,132 +118,154 @@
 
 
 // 3 - Halvsies
+// function halvsies(array) {
+//   let arr1 = [];
+//   let length = array.length;
+//   if (length % 2 === 1) {
+//     for (let i = 0; i < Math.ceil(length / 2); i++) {
+//       arr1.push(array.shift());
+//     }
+//   } else {
+//     for (let i = 0; i < (length / 2); i++) {
+//       arr1.push(array.shift());
+//     }
+//   }
+//   return [arr1, array];
+// }
+
+// console.log(halvsies([1, 2, 3, 4]));       // [[1, 2], [3, 4]]
+// console.log(halvsies([1, 5, 2, 4, 3]));    // [[1, 5, 2], [4, 3]]
+// console.log(halvsies([5]));                // [[5], []]
+// console.log(halvsies([]));                 // [[], []]
 
 
 
+// Launch School Solution
+// function halvsies(array) {
+//   let half = Math.ceil(array.length / 2);
+//   let firstHalf = array.slice(0, half);
+//   let secondHalf = array.slice(half);
+//   return [firstHalf, secondHalf];
+// }
 
-
-
-
-
-
-
-
-
-
-
-
+// console.log(halvsies([1, 2, 3, 4]));       // [[1, 2], [3, 4]]
+// console.log(halvsies([1, 5, 2, 4, 3]));    // [[1, 5, 2], [4, 3]]
+// console.log(halvsies([5]));                // [[5], []]
+// console.log(halvsies([]));                 // [[], []]
 
 
 
 
 
 // 4 - Find the Duplicate
+// function findDup(array) {
+//   let newArray = [];
+//   array.forEach(x => {
+//     if (newArray.includes(x)) {
+//       console.log(x);
+//     }
+//     newArray.push(x);
+//   });
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// findDup([1, 5, 3, 1]);                                // 1
+// findDup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+//          38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+//          14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+//          78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+//          89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+//          41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+//          55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+//          85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+//          40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+//           7, 34, 57, 74, 45, 11, 88, 67,  5, 58]);    // 73
 
 
 
 
 
 // 5 - Combine Two Lists
+// function interleave(arr1, arr2) {
+//   let newArr = [];
+//   for (let i = 0; i < arr1.length; i++) {
+//     newArr.push(arr1[i], arr2[i]);
+//   }
+//   return newArr;
+// }
 
+// console.log(interleave([1, 2, 3], ['a', 'b', 'c']));    // [1, "a", 2, "b", 3, "c"]
 
+// Further Exploration 1
+// function interleave(arr1, arr2) {
+//   let newArr = [];
+//   arr1.forEach((x, index) => newArr.push(x, arr2[index]));
+//   return newArr;
+// }
 
+// console.log(interleave([1, 2, 3], ['a', 'b', 'c']));    // [1, "a", 2, "b", 3, "c"]
 
+// Further Exploration 2
+// function interleave(arr1, arr2) {
+//   return arr1.map((x, index) => [x, arr2[index]]).flat();
+// }
 
+// console.log(interleave([1, 2, 3], ['a', 'b', 'c']));    // [1, "a", 2, "b", 3, "c"]
 
+// Further Exploration 3
+// function interleave(arr1, arr2) {
+//   return arr1.reduce((acc, currentValue, index) => {
+//     acc.push(currentValue, arr2[index]);
+//     return acc;
+//   }, []);  
+// }
 
-
-
-
-
-
-
-
-
+// console.log(interleave([1, 2, 3], ['a', 'b', 'c']));    // [1, "a", 2, "b", 3, "c"]
 
 
 
 
 
 // 6 - Multiplicative Average
+// function multiplicativeAverage(array) {
+//   return ((array.reduce((acc, currentValue) => acc * currentValue, 1)) / (array.length)).toFixed(3);
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(multiplicativeAverage([3, 5]));                   // "7.500"
+// console.log(multiplicativeAverage([2, 5, 7, 11, 13, 17]));    // "28361.667"
 
 
 
 
 
 // 7 - Multiply Lists
+// function multiplyList(arr1, arr2) {
+//   return arr1.map((x, index) => x * arr2[index]);
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(multiplyList([3, 5, 7], [9, 10, 11]));    // [27, 50, 77]
 
 
 
 
 
 // 8 - List of Digits
+// function digitList(num) {
+//   return num.toString().split('').map((x) => Number.parseInt(x));
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(digitList(12345));       // [1, 2, 3, 4, 5]
+// console.log(digitList(7));           // [7]
+// console.log(digitList(375290));      // [3, 7, 5, 2, 9, 0]
+// console.log(digitList(444));         // [4, 4, 4]
 
 
 
 
 
 // 9 - How Many?
+function countOccurrences() {
+  
+}
 
 
 
@@ -251,8 +273,17 @@
 
 
 
+let vehicles = ['car', 'car', 'truck', 'car', 'SUV', 'truck',
+  'motorcycle', 'suv', 'motorcycle', 'car', 'truck'];
 
+countOccurrences(vehicles);
 
+// console output -- your output sequence may be different
+// car => 4
+// truck => 3
+// SUV => 1
+// motorcycle => 2
+// suv => 1
 
 
 
