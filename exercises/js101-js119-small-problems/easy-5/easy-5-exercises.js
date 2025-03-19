29f27f45f479e57f538022779f147751022ccbb4
Austin Kim <austinkim61@gmail.com>
2025-03-18 22:31:25 -0400

    finished easy 5

--- exercises/js101-js119-small-problems/easy-5/easy-5-exercises.js
+++ exercises/js101-js119-small-problems/easy-5/easy-5-exercises.js
@@ -400,58 +400,133 @@
 // console.log(timeOfDay(-4231));
 
 // Further Exploration
-const MINUTES_PER_HOUR = 60;
-const HOURS_PER_DAY = 24;
-const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+// function timeOfDay(deltaMinutes) {  
+//   let date = new Date();
 
-function leadingZero(number) {
-  return number < 10 ? `0${number}` : String(number);
-}
+//   date.setUTCHours(0, 0, 0, 0);
+//   date.setUTCMinutes(date.getUTCMinutes() + deltaMinutes);
 
-function formatTime(hours, minutes) {
-  return `${leadingZero(hours)}:${leadingZero(minutes)}`;
-}
+//   return `${date.getUTCHours().toString().padStart(2, '0')}:${date.getUTCMinutes().toString().padStart(2, '0')}` 
+// }
 
-function timeOfDay(deltaMinutes) {
-  if (deltaMinutes < 0) {
-    deltaMinutes = (deltaMinutes % MINUTES_PER_DAY) + MINUTES_PER_DAY;
-  } else {
-    deltaMinutes = deltaMinutes % MINUTES_PER_DAY;
-  }
-
-  let hours = Math.floor(deltaMinutes / MINUTES_PER_HOUR);
-  let minutes = deltaMinutes % MINUTES_PER_HOUR;
-
-  return formatTime(hours, minutes);
-}
-
-
+// console.log(timeOfDay(0));
+// console.log(timeOfDay(-3));
+// console.log(timeOfDay(35));
+// console.log(timeOfDay(-1437));
+// console.log(timeOfDay(3000));
+// console.log(timeOfDay(800));
+// console.log(timeOfDay(-4231));
 
 
 
 
 
 // 12 - After Midnight (Part 2)
+// const MINUTES_PER_HOUR = 60;
+// const HOURS_PER_DAY = 24;
+// const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+
+// function afterMidnight(string) {
+//   let array = string.split(':');
+//   let hours = parseInt(array[0]);
+//   let minutes = parseInt(array[1]);
+//   let result = ((hours % HOURS_PER_DAY) * MINUTES_PER_HOUR) + minutes;
+
+//   return result;
+// }
+
+// function beforeMidnight(string) {
+//   let array = string.split(':');  
+//   let hours = parseInt(array[0]);
+//   let minutes = parseInt(array[1]);
+//   let result = ((hours % HOURS_PER_DAY) * MINUTES_PER_HOUR) + minutes;
+
+//   return Math.abs((result - MINUTES_PER_DAY) % MINUTES_PER_DAY);
+// }
+
+// function beforeMidnight(string) {
+//   let array = string.split(':');  
+//   let hours = parseInt(array[0]);
+//   let minutes = parseInt(array[1]);
+//   let result = ((hours % 24) * 60) - 60 + (60 - minutes);
+//   return result;
+
+// }
+
+// console.log(afterMidnight("00:00"));
+// console.log(beforeMidnight("00:00"));
+// console.log(afterMidnight("12:34"));
+// console.log(beforeMidnight("12:34"));
+// console.log(afterMidnight("24:00"));
+// console.log(beforeMidnight("24:00"));
+
+// console.log(afterMidnight("00:00") === 0);
+// console.log(beforeMidnight("00:00") === 0);
+// console.log(afterMidnight("12:34") === 754);
+// console.log(beforeMidnight("12:34") === 686);
+// console.log(afterMidnight("24:00") === 0);
+// console.log(beforeMidnight("24:00") === 0);
 
 
 
+// Launch School Solution
+// const HOURS_PER_DAY = 24;
+// const MINUTES_PER_HOUR = 60;
+// const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+
+// function afterMidnight(timeStr) {
+//   let [hours, minutes] = timeStr.split(":").map(num => Number(num));
+//   return ((hours * MINUTES_PER_HOUR) + minutes) % MINUTES_PER_DAY;
+// }
+
+// function beforeMidnight(timeStr) {
+//   let deltaMinutes = MINUTES_PER_DAY - afterMidnight(timeStr);
+//   if (deltaMinutes === MINUTES_PER_DAY) {
+//     deltaMinutes = 0;
+//   }
+//   return deltaMinutes;
+// }
+
+// console.log(afterMidnight("00:00"));
+// console.log(beforeMidnight("00:00"));
+// console.log(afterMidnight("12:34"));
+// console.log(beforeMidnight("12:34"));
+// console.log(afterMidnight("24:00"));
+// console.log(beforeMidnight("24:00"));
 
 
 
+// Further Exploration
+// const HOURS_PER_DAY = 24;
+// const MINUTES_PER_HOUR = 60;
+// const SECONDS_PER_MINUTE = 60;
+// const MILLISECONDS_PER_SECOND = 1000;
+// const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
 
+// function afterMidnight(timeStr) {
+//   let [hours, minutes] = timeStr.split(":").map(num => Number(num));
+//   let date = new Date();
+//   let midnight = new Date();
 
+//   date.setUTCHours(hours, minutes, 0, 0);
+//   midnight.setUTCHours(0, 0, 0, 0);
 
+//   let results = ((date.getTime() - midnight.getTime()) / (SECONDS_PER_MINUTE * MILLISECONDS_PER_SECOND)) % MINUTES_PER_DAY;  
+//   return results;
 
+// }
 
+// function beforeMidnight(timeStr) {
+//   let deltaMinutes = MINUTES_PER_DAY - afterMidnight(timeStr);
+//   if (deltaMinutes === MINUTES_PER_DAY) {
+//     deltaMinutes = 0;
+//   }
+//   return deltaMinutes;
+// }
 
-
-
-
-
-
-
-
-
-
-
-
+// console.log(afterMidnight("00:00"));
+// console.log(beforeMidnight("00:00"));
+// console.log(afterMidnight("12:34"));
+// console.log(beforeMidnight("12:34"));
+// console.log(afterMidnight("24:00"));
+// console.log(beforeMidnight("24:00"));
