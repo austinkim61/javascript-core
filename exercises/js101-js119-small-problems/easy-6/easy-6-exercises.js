84a5f0f401b0f32c63fd98cacd79776a2ab207c2
Austin Kim <austinkim61@gmail.com>
2025-03-18 22:37:16 -0400

    prepared easy 6

--- exercises/js101-js119-small-problems/easy-6/easy-6-exercises.js
+++ exercises/js101-js119-small-problems/easy-6/easy-6-exercises.js
@@ -1,217 +1,171 @@
 // 1 - Double Char (Part 1)
+function repeater(string) {
+
+}
 
 
 
-
-
-
-
-
-
-
-
-
+repeater('Hello');        // "HHeelllloo"
+repeater('Good job!');    // "GGoooodd  jjoobb!!"
+repeater('');             // ""
 
 
 
 
 
 // 2 - Double Char (Part 2)
+function doubleConsonants(string) {
+  
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
+doubleConsonants('String');          // "SSttrrinngg"
+doubleConsonants('Hello-World!');    // "HHellllo-WWorrlldd!"
+doubleConsonants('July 4th');        // "JJullyy 4tthh"
+doubleConsonants('');                // ""
 
 
 
 
 
 // 3 - Reverse Number
+function reverseNumber(num) {
+  
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+reverseNumber(12345);    // 54321
+reverseNumber(12213);    // 31221
+reverseNumber(456);      // 654
+reverseNumber(12000);    // 21 -- Note that leading zeros in the result get dropped!
+reverseNumber(1);        // 1
 
 
 
 
 
 // 4 - Counting Up
+function sequence(num) {
+  
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sequence(5);    // [1, 2, 3, 4, 5]
+sequence(3);    // [1, 2, 3]
+sequence(1);    // [1]
 
 
 
 
 
 // 5 - Name Swapping
+function swapName(string) {
+  
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+swapName('Joe Roberts');    // "Roberts, Joe"
 
 
 
 
 
 // 6 - Sequence Count
+function sequence(num1, num2) {
+  
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+sequence(5, 1);          // [1, 2, 3, 4, 5]
+sequence(4, -7);         // [-7, -14, -21, -28]
+sequence(3, 0);          // [0, 0, 0]
+sequence(0, 1000000);    // []
 
 
 
 
 
 // 7 - Reverse It (Part 1)
+function reverseSentence(string) {
+  
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+reverseSentence('');                       // ""
+reverseSentence('Hello World');            // "World Hello"
+reverseSentence('Reverse these words');    // "words these Reverse"
 
 
 
 
 
 // 8 - Reverse It (Part 2)
+function reverseWords(string) {
+  
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+reverseWords('Professional');             // "lanoisseforP"
+reverseWords('Walk around the block');    // "Walk dnuora the kcolb"
+reverseWords('Launch School');            // "hcnuaL loohcS"
 
 
 
 
 
 // 9 - Reversed Arrays
+function reverse(array) {
+  
+}
 
 
 
+let list = [1, 2, 3, 4];
+let result = reverse(list);
+console.log(result); // logs [4,3,2,1]
+console.log(list === result); // logs true
 
+let list1 = ["a", "b", "c", "d", "e"];
+let result1 = reverse(list1);
+console.log(result1); // logs  ["e", "d", "c", "b", "a"]
+console.log(list1 === result1); // logs true
 
+let list2 = ["abc"];
+let result2 = reverse(list2);
+console.log(result2); // logs  ["abc"]
+console.log(list2 === result2); // logs true
 
-
-
-
-
-
-
-
-
-
-
-
+let list3 = [];
+let result3 = reverse(list3);
+console.log(result3); // logs []
+console.log(list3 === result3); // logs true
 
 
 
 
 
 // 10 - Matching Parentheses?
+function isBalanced(string) {
+  
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(isBalanced("What (is) this?") === true);
+console.log(isBalanced("What is) this?") === false);
+console.log(isBalanced("What (is this?") === false);
+console.log(isBalanced("((What) (is this))?") === true);
+console.log(isBalanced("((What)) (is this))?") === false);
+console.log(isBalanced("Hey!") === true);
+console.log(isBalanced(")Hey!(") === false);
+console.log(isBalanced("What ((is))) up(") === false);
 
 
 
