683f1350a1076d942fb66450fc88f36fb820566f
Austin Kim <austinkim61@gmail.com>
2025-03-20 19:00:37 -0400

    updated notes

--- exercises/js101-js119-small-problems/easy-6/easy-6-exercises.js
+++ exercises/js101-js119-small-problems/easy-6/easy-6-exercises.js
@@ -55,15 +55,15 @@
 
 
 // 3 - Reverse Number
-function reverseNumber(num) {
-  return parseInt(num.toString().split('').reverse().join(''), 10);
-}
+// function reverseNumber(num) {
+//   return parseInt(num.toString().split('').reverse().join(''), 10);
+// }
 
-console.log(reverseNumber(12345));    // 54321
-console.log(reverseNumber(12213));    // 31221
-console.log(reverseNumber(456));      // 654
-console.log(reverseNumber(12000));    // 21 -- Note that leading zeros in the result get dropped!
-console.log(reverseNumber(1));        // 1
+// console.log(reverseNumber(12345));    // 54321
+// console.log(reverseNumber(12213));    // 31221
+// console.log(reverseNumber(456));      // 654
+// console.log(reverseNumber(12000));    // 21 -- Note that leading zeros in the result get dropped!
+// console.log(reverseNumber(1));        // 1
 
 
 
@@ -71,14 +71,37 @@ console.log(reverseNumber(1));        // 1
 
 // 4 - Counting Up
 // function sequence(num) {
-  
+//   let array = [];
+//   for (let i = 1; i <= num; i++) {
+//     array.push(i);
+//   }
+
+//   return array;  
 // }
 
+// console.log(sequence(5));    // [1, 2, 3, 4, 5]
+// console.log(sequence(3));    // [1, 2, 3]
+// console.log(sequence(1));    // [1]
 
+// Further Exploration
+// function sequence(num) {
+//   let array = [...Array(num)];
+//   array.forEach((_, index) => array[index] = index + 1);
 
-// sequence(5);    // [1, 2, 3, 4, 5]
-// sequence(3);    // [1, 2, 3]
-// sequence(1);    // [1]
+//   return array;
+// }
+
+// console.log(sequence(5));    // [1, 2, 3, 4, 5]
+// console.log(sequence(3));    // [1, 2, 3]
+// console.log(sequence(1));    // [1]
+
+// function sequence(num) {
+//   return [...Array(num)].map((_, index) => index + 1);
+// }
+
+// console.log(sequence(5));    // [1, 2, 3, 4, 5]
+// console.log(sequence(3));    // [1, 2, 3]
+// console.log(sequence(1));    // [1]
 
 
 
@@ -86,28 +109,37 @@ console.log(reverseNumber(1));        // 1
 
 // 5 - Name Swapping
 // function swapName(string) {
-  
+//   return string.split(' ').reverse().join(', ');
 // }
 
+// console.log(swapName('Joe Roberts'));    // "Roberts, Joe"
 
+// Further Exploration
+// function swapName(string) {
+//   let array = string.split(' ');
+//   if (array.length > 2) {
+//     array.unshift(`${array.pop()},`);
+//     return array.join(' ');
+//   } else {
+//     return array.reverse().join(', ');
+//   }
+// }
 
-// swapName('Joe Roberts');    // "Roberts, Joe"
+// console.log(swapName('Karl Oskar Henriksson Ragvals'));    // "Ragvals, Karl Oskar Henriksson"
 
 
 
 
 
 // 6 - Sequence Count
-// function sequence(num1, num2) {
+function sequence(num1, num2) {
   
-// }
+}
 
-
-
-// sequence(5, 1);          // [1, 2, 3, 4, 5]
-// sequence(4, -7);         // [-7, -14, -21, -28]
-// sequence(3, 0);          // [0, 0, 0]
-// sequence(0, 1000000);    // []
+sequence(5, 1);          // [1, 2, 3, 4, 5]
+sequence(4, -7);         // [-7, -14, -21, -28]
+sequence(3, 0);          // [0, 0, 0]
+sequence(0, 1000000);    // []
 
 
 
@@ -117,8 +149,6 @@ console.log(reverseNumber(1));        // 1
 // function reverseSentence(string) {
   
 // }
-
-
 
 // reverseSentence('');                       // ""
 // reverseSentence('Hello World');            // "World Hello"
@@ -133,8 +163,6 @@ console.log(reverseNumber(1));        // 1
   
 // }
 
-
-
 // reverseWords('Professional');             // "lanoisseforP"
 // reverseWords('Walk around the block');    // "Walk dnuora the kcolb"
 // reverseWords('Launch School');            // "hcnuaL loohcS"
@@ -147,8 +175,6 @@ console.log(reverseNumber(1));        // 1
 // function reverse(array) {
   
 // }
-
-
 
 // let list = [1, 2, 3, 4];
 // let result = reverse(list);
@@ -179,8 +205,6 @@ console.log(reverseNumber(1));        // 1
   
 // }
 
-
-
 // console.log(isBalanced("What (is) this?") === true);
 // console.log(isBalanced("What is) this?") === false);
 // console.log(isBalanced("What (is this?") === false);
@@ -189,13 +213,3 @@ console.log(reverseNumber(1));        // 1
 // console.log(isBalanced("Hey!") === true);
 // console.log(isBalanced(")Hey!(") === false);
 // console.log(isBalanced("What ((is))) up(") === false);
-
-
-
-
-
-
-
-
-
-
