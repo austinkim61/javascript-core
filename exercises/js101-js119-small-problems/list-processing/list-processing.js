78ec9664422ba8250762cd5f8f89b0b9a14da504
Austin Kim <austinkim61@gmail.com>
2025-03-22 18:05:59 -0400

    readied file list processing

--- exercises/js101-js119-small-problems/list-processing/list-processing.js
+++ exercises/js101-js119-small-problems/list-processing/list-processing.js
@@ -1,42 +1,160 @@
-// 1 - 
-// 2 - 
-// 3 - 
-// 4 - 
-// 5 - 
-// 6 - 
-// 7 - 
-// 8 - 
-// 9 - 
-// 10 - 
-// 11 - 
-// 12 - 
+// 1 - Sum Of Digits
+function sum(integer) {
+
+}
+
+console.log(sum(23));           // 5
+console.log(sum(496));          // 19
+console.log(sum(123456789));    // 45
 
 
 
 
 
+// 2 - Alphabetical Numbers
+// function () {
+  
+// }
+
+// console.log(alphabeticNumberSort(
+//   [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]));
+// [8, 18, 11, 15, 5, 4, 14, 9, 19, 1, 7, 17, 6, 16, 10, 13, 3, 12, 2, 0]
 
 
 
 
 
+// 3 - Multiply All Pairs
+// function multiplyAllPairs(arr1, arr2) {
+  
+// }
+
+// console.log(multiplyAllPairs([2, 4], [4, 3, 1, 2]));    // [2, 4, 4, 6, 8, 8, 12, 16]
 
 
 
 
 
+// 4 - Leading Substrings
+// function leadingSubstrings(string) {
+  
+// }
+
+// console.log(leadingSubstrings('abc'));      // ["a", "ab", "abc"]
+// console.log(leadingSubstrings('a'));        // ["a"]
+// console.log(leadingSubstrings('xyzzy'));    // ["x", "xy", "xyz", "xyzz", "xyzzy"]
 
 
 
 
 
+// 5 - All Substrings
+// function substrings(string) {
+  
+// }
+
+// console.log(substrings('abcde'));
+
+// returns
+/* [ "a", "ab", "abc", "abcd", "abcde",
+  "b", "bc", "bcd", "bcde",
+  "c", "cd", "cde",
+  "d", "de",
+  "e" ]
+*/
 
 
 
 
 
+// 6 - Palindromic Substrings
+// function palindoromes(string) {
+  
+// }
+
+// console.log(palindromes('abcd'));       // []
+// console.log(palindromes('madam'));      // [ "madam", "ada" ]
+// console.log(palindromes('hello-madam-did-madam-goodbye'));
+// returns
+// [ "ll", "-madam-", "-madam-did-madam-", "madam", "madam-did-madam", "ada",
+//   "adam-did-mada", "dam-did-mad", "am-did-ma", "m-did-m", "-did-", "did",
+//   "-madam-", "madam", "ada", "oo" ]
+// console.log(palindromes('knitting cassettes'));
+// returns
+// [ "nittin", "itti", "tt", "ss", "settes", "ette", "tt" ]
 
 
 
 
 
+// 7 - Sum of Sums
+// function sumOfSums(array) {
+  
+// }
+
+// console.log(sumOfSums([3, 5, 2]));        // (3) + (3 + 5) + (3 + 5 + 2) --> 21
+// console.log(sumOfSums([1, 5, 7, 3]));     // (1) + (1 + 5) + (1 + 5 + 7) + (1 + 5 + 7 + 3) --> 36
+// console.log(sumOfSums([4]));              // 4
+// console.log(sumOfSums([1, 2, 3, 4, 5]));  // 35
+
+
+
+
+
+// 8 - Grocery List
+// function buyFruit(...arrays) {
+  
+// }
+
+// console.log(buyFruit([['apple', 3], ['orange', 1], ['banana', 2]]));
+// returns ["apple", "apple", "apple", "orange", "banana", "banana"]
+
+
+
+
+
+// 9 - Inventory Item Transactions
+// let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+//   { id: 105, movement: 'in',  quantity: 10 },
+//   { id: 102, movement: 'out', quantity: 17 },
+//   { id: 101, movement: 'in',  quantity: 12 },
+//   { id: 103, movement: 'out', quantity: 20 },
+//   { id: 102, movement: 'out', quantity: 15 },
+//   { id: 105, movement: 'in',  quantity: 25 },
+//   { id: 101, movement: 'out', quantity: 18 },
+//   { id: 102, movement: 'in',  quantity: 22 },
+//   { id: 103, movement: 'out', quantity: 15 }, ];
+
+// function transactionsFor(itemID, listOfTransactions) {
+  
+// }
+
+// console.log(transactionsFor(101, transactions));
+// returns
+// [ { id: 101, movement: "in",  quantity:  5 },
+//   { id: 101, movement: "in",  quantity: 12 },
+//   { id: 101, movement: "out", quantity: 18 }, ]
+
+
+
+
+
+// 10 - Inventory Item Availability
+// let transactions = [ { id: 101, movement: 'in',  quantity:  5 },
+//   { id: 105, movement: 'in',  quantity: 10 },
+//   { id: 102, movement: 'out', quantity: 17 },
+//   { id: 101, movement: 'in',  quantity: 12 },
+//   { id: 103, movement: 'out', quantity: 20 },
+//   { id: 102, movement: 'out', quantity: 15 },
+//   { id: 105, movement: 'in',  quantity: 25 },
+//   { id: 101, movement: 'out', quantity: 18 },
+//   { id: 102, movement: 'in',  quantity: 22 },
+//   { id: 103, movement: 'out', quantity: 15 }, ];
+
+// function isItemAvailable(itemID, listOfTransactions) {
+  
+// }
+
+// console.log(isItemAvailable(101, transactions));     // false
+// console.log(isItemAvailable(103, transactions));     // false
+// console.log(isItemAvailable(105, transactions));     // true
