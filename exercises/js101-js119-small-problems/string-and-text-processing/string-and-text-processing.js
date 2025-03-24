68f0ed6d4bf56f339b448b48761bba50a93c0a42
Austin Kim <austinkim61@gmail.com>
2025-03-24 00:09:54 -0400

    getting string and text processing ready

--- exercises/js101-js119-small-problems/string-and-text-processing/string-and-text-processing.js
+++ exercises/js101-js119-small-problems/string-and-text-processing/string-and-text-processing.js
@@ -1,27 +1,146 @@
-// 1 - 
-// 2 - 
-// 3 - 
-// 4 - 
-// 5 - 
-// 6 - 
-// 7 - 
-// 8 - 
-// 9 - 
-// 10 - 
-// 11 - 
-// 12 - 
+// 1 - Uppercase Check
+function isUppercase(string) {
+  
+}
+
+console.log(isUppercase('t'));               // false
+console.log(isUppercase('T'));               // true
+console.log(isUppercase('Four Score'));      // false
+console.log(isUppercase('FOUR SCORE'));      // true
+console.log(isUppercase('4SCORE!'));         // true
+console.log(isUppercase(''));                // true
 
 
 
 
 
+// 2 - Delete Vowels
+// function removeVowels(array) {
+
+// }
+
+// console.log(removeVowels(['abcdefghijklmnopqrstuvwxyz']));         // ["bcdfghjklmnpqrstvwxyz"]
+// console.log(removeVowels(['green', 'YELLOW', 'black', 'white']));  // ["grn", "YLLW", "blck", "wht"]
+// console.log(removeVowels(['ABC', 'AEIOU', 'XYZ']));                // ["BC", "", "XYZ"]
 
 
 
 
 
+// 3 - Lettercase Counter
+// function letterCaseCount(string) {
+  
+// }
+
+// console.log(letterCaseCount('abCdef 123'));  // { lowercase: 5, uppercase: 1, neither: 4 }
+// console.log(letterCaseCount('AbCd +Ef'));    // { lowercase: 3, uppercase: 3, neither: 2 }
+// console.log(letterCaseCount('123'));         // { lowercase: 0, uppercase: 0, neither: 3 }
+// console.log(letterCaseCount(''));            // { lowercase: 0, uppercase: 0, neither: 0 }
 
 
+
+
+
+// 4 - Capitalize Words
+// function wordCap(string) {
+
+// }
+
+// console.log(wordCap('four score and seven'));       // "Four Score And Seven"
+// console.log(wordCap('the javaScript language'));    // "The Javascript Language"
+// console.log(wordCap('this is a "quoted" word'));    // 'This Is A "quoted" Word'
+
+
+
+
+
+// 5 - Swap Case
+// function swapCase(string) {
+  
+// }
+
+// console.log(swapCase('CamelCase'));              // "cAMELcASE"
+// console.log(swapCase('Tonight on XYZ-TV'));      // "tONIGHT ON xyz-tv"
+
+
+
+
+
+// 6 - Staggered Caps (Part 1)
+// function staggeredCase(string) {
+  
+// }
+
+// console.log(staggeredCase('I Love Launch School!'));        // "I LoVe lAuNcH ScHoOl!"
+// console.log(staggeredCase('ALL_CAPS'));                     // "AlL_CaPs"
+// console.log(staggeredCase('ignore 77 the 4444 numbers'));   // "IgNoRe 77 ThE 4444 nUmBeRs"
+
+
+
+
+
+// 7 - Staggered Caps (Part 2)
+// function staggeredCase(string) {
+  
+// }
+
+// console.log(staggeredCase("I Love Launch School!") === "I lOvE lAuNcH sChOoL!");
+// console.log(staggeredCase("ALL CAPS") === "AlL cApS");
+// console.log(
+//   staggeredCase("ignore 77 the 444 numbers") === "IgNoRe 77 ThE 444 nUmBeRs"
+// );
+
+
+
+
+
+// 8 - How long are you?
+// function wordLengths(string) {
+  
+// }
+
+// wordLengths('cow sheep chicken');
+// // ["cow 3", "sheep 5", "chicken 7"]
+
+// wordLengths('baseball hot dogs and apple pie');
+// // ["baseball 8", "hot 3", "dogs 4", "and 3", "apple 5", "pie 3"]
+
+// wordLengths("It ain't easy, is it?");
+// // ["It 2", "ain't 5", "easy, 5", "is 2", "it? 3"]
+
+// wordLengths('Supercalifragilisticexpialidocious');
+// // ["Supercalifragilisticexpialidocious 34"]
+
+// console.log(wordLengths(''));      // []
+// console.log(wordLengths());        // []
+
+
+
+
+
+// 9 - Search Word (Part 1)
+// function searchWord(str, txt) {
+
+// }
+
+// const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?';
+
+// console.log(searchWord('sed', text));      // 3
+
+
+
+
+
+// 10 - Search Word (Part 2)
+// function searchWord(str, txt) {
+  
+// }
+
+// const text = 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Sed quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?';
+
+// console.log(searchWord('sed', text));
+// returns
+// "**SED** ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, **SED** quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, **SED** quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? **SED** quis autem vel est, iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur blasedbla?"
 
 
 
