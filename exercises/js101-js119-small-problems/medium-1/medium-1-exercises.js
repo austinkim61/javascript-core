d8ae81ed494f6023b73acc6a3d38d63038b6548f
Austin Kim <austinkim61@gmail.com>
2025-03-25 23:05:52 -0400

    stopped on problem 4

--- exercises/js101-js119-small-problems/medium-1/medium-1-exercises.js
+++ exercises/js101-js119-small-problems/medium-1/medium-1-exercises.js
@@ -73,94 +73,118 @@
 
 
 // 3 - Rotation (Part 3)
-function maxRotation(number) {
-  let result = '';
-  let string = String(number);
+// function maxRotation(number) {
+//   for (let i = String(number).length; i > 0; i--) {
+//     number = rotateRightmostDigits(number, i);
+//   }
+//   return number;
+// }
 
-  console.log(rotateRightmostDigits(number, string.length));
+// function rotateRightmostDigits(number, count) {
+//   let numberString = String(number);
+//   let firstPart = numberString.slice(0, numberString.length - count); 
+//   let secondPart = numberString.slice(numberString.length - count);
+//   let resultString = firstPart + rotateString(secondPart);
 
-  for (let i = 0; i < string.length - 1; i++) {
-    rotateRightmostDigits
+//   return Number(resultString);
+// }
 
-  } 
-}
+// function rotateString(string) {
+//   return string.slice(1) + string[0];
+// }
 
-
-
-function rotateRightmostDigits(number, count) {
-  let numberString = String(number); // 735291
-  let firstPart = numberString.slice(0, numberString.length - count); // 0, 6 - 3 => 0, 3 => indices 0 to 2 => '735' 
-  let secondPart = numberString.slice(numberString.length - count); // 6 - 3, => 3 => indices 3 to end => 291
-  let resultString = firstPart + rotateString(secondPart);
-
-  return Number(resultString);
-}
-
-function rotateString(string) { // '291'
-  return string.slice(1) + string[0]; // '91' + '2'
-}
-
+// console.log(maxRotation(735291));          // 321579
+// console.log(maxRotation(3));               // 3
+// console.log(maxRotation(35));              // 53
+// console.log(maxRotation(105));             // 15 -- the leading zero gets dropped
+// console.log(maxRotation(8703529146));      // 7321609845
 
 
+// function maxRotation(number) {
+//   for (let i = String(number).length; i > 0; i--) {
+//     number = rotateRightmostDigits(number, i);
+//   }
+//   return number;
+// }
+
+// function rotateRightmostDigits(digit, count) {
+//   let array = digit.toString().split('');
+//   let lastDigits = array.splice(1 - count);
+  
+//   array.splice(array.length - 1, 0, lastDigits);
+
+//   return Number(array.flat().join(''));  
+// }
+
+// console.log(maxRotation(735291));          // 321579
+// console.log(maxRotation(3));               // 3
+// console.log(maxRotation(35));              // 53
+// console.log(maxRotation(105));             // 15 -- the leading zero gets dropped
+// console.log(maxRotation(8703529146));      // 7321609845
 
 
-
-console.log(maxRotation(735291));          // 321579
-console.log(maxRotation(3));               // 3
-console.log(maxRotation(35));              // 53
-console.log(maxRotation(105));             // 15 -- the leading zero gets dropped
-console.log(maxRotation(8703529146));      // 7321609845
-
-// 1st 0 digits fixed, 1st non fixed digit (1st) to the end
-// 735291 // original
-// 352917 // 1st 0 digits fixed, 1st non fixed digit (1st) - 7 to the end
-// 329175 // 1st 1 digits fixed, 1st non fixed digit (2nd) - 5 to the end
-// 321759 // 1st 2 digits fixed, 1st non fixed digit (3rd) - 9 to the end
-// 321597 // 1st 3 digits fixed, 1st non fixed digit (4th) - 7 to the end
-// 321579 // 1st 4 digits fixed, 1st non fixed digit (5th) - 9 to the end
-// 
-// rotations = string.length - 1
 
 
 
 // 4 - Stack Machine Interpretation
-// function minilang(string) {
+function minilang(string) {
   
-// }
+}
 
-// console.log(minilang('PRINT'));
-// // 0
+console.log(minilang('PRINT'));
+// 0
 
-// console.log(minilang('5 PUSH 3 MULT PRINT'));
-// // 15
+console.log(minilang('5 PUSH 3 MULT PRINT'));
+// 15
 
-// console.log(minilang('5 PRINT PUSH 3 PRINT ADD PRINT'));
-// // 5
-// // 3
-// // 8
+console.log(minilang('5 PRINT PUSH 3 PRINT ADD PRINT'));
+// 5
+// 3
+// 8
 
-// console.log(minilang('5 PUSH POP PRINT'));
-// // 5
+console.log(minilang('5 PUSH POP PRINT'));
+// 5
 
-// console.log(minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT'));
-// // 5
-// // 10
-// // 4
-// // 7
+console.log(minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT'));
+// 5
+// 10
+// 4
+// 7
 
-// console.log(minilang('3 PUSH PUSH 7 DIV MULT PRINT'));
-// // 6
+console.log(minilang('3 PUSH PUSH 7 DIV MULT PRINT'));
+// 6
 
-// console.log(minilang('4 PUSH PUSH 7 REMAINDER MULT PRINT'));
-// // 12
+console.log(minilang('4 PUSH PUSH 7 REMAINDER MULT PRINT'));
+// 12
 
-// console.log(minilang('-3 PUSH 5 SUB PRINT'));
-// // 8
+console.log(minilang('-3 PUSH 5 SUB PRINT'));
+// 8
 
-// console.log(minilang('6 PUSH'));
-// // (nothing is printed because the `program` argument has no `PRINT` commands)
+console.log(minilang('6 PUSH'));
+// (nothing is printed because the `program` argument has no `PRINT` commands)
 
 
+
+
+/*
+n : Place a value, n, in the register. Do not modify the stack.
+PUSH : Push the register value onto the stack. Leave the value in the register.
+ADD : Pop a value from the stack and add it to the register value, storing the result in the register.
+SUB : Pop a value from the stack and subtract it from the register value, storing the result in the register.
+MULT : Pop a value from the stack and multiply it by the register value, storing the result in the register.
+DIV : Pop a value from the stack and divide the register value by the popped stack value, storing the integer result back in the register.
+REMAINDER : Pop a value from the stack and divide the register value by the popped stack value, storing the integer remainder of the division back in the register.
+POP : Remove the topmost item from the stack and place it in the register.
+PRINT : Print the register value.
+
+      '3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT'
+                              5         10        4         7
+register (current value):  3 4 5 10 4 7
+[stack]:                   
+
+
+
+*/
 
 
 
