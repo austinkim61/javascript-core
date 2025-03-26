1da8b6e6ca802c6377104f17b622d08b8c32c003
Austin Kim <austinkim61@gmail.com>
2025-03-25 21:39:37 -0400

    started medium 1

--- exercises/js101-js119-small-problems/medium-1/medium-1-exercises.js
+++ exercises/js101-js119-small-problems/medium-1/medium-1-exercises.js
@@ -1,6 +1,12 @@
 // 1 - Rotation (Part 1)
-// function rotateArray(array) {
+// let array = [1, 2, 3, 4];
 
+// function rotateArray(args) {
+//   if (!Array.isArray(args)) {
+//     return undefined;
+//   }
+
+//   return [].concat(args.filter((_, index) => index !== 0), args.filter((_, index) => index === 0));
 // }
 
 // console.log(rotateArray([7, 3, 5, 2, 9, 1]));       // [3, 5, 2, 9, 1, 7]
@@ -14,11 +20,9 @@
 // console.log(rotateArray());                         // undefined
 // console.log(rotateArray(1));                        // undefined
 
-
 // // the input array is not mutated
-// let array = [1, 2, 3, 4];
-// rotateArray(array);                    // [2, 3, 4, 1]
-// array;                                 // [1, 2, 3, 4]
+// console.log(rotateArray(array));                    // [2, 3, 4, 1]
+// console.log(array);                                 // [1, 2, 3, 4]
 
 
 
@@ -26,7 +30,35 @@
 
 // 2 - Rotation (Part 2)
 // function rotateRightmostDigits(digit, count) {
+//   let array = digit.toString().split('');
+//   let lastDigits = array.splice(1 - count);
   
+//   array.splice(array.length - 1, 0, lastDigits);
+
+//   return Number(array.flat().join(''));  
+// }
+
+// console.log(rotateRightmostDigits(735291, 1));      // 735291
+// console.log(rotateRightmostDigits(735291, 2));      // 735219
+// console.log(rotateRightmostDigits(735291, 3));      // 735912
+// console.log(rotateRightmostDigits(735291, 4));      // 732915
+// console.log(rotateRightmostDigits(735291, 5));      // 752913
+// console.log(rotateRightmostDigits(735291, 6));      // 352917
+
+
+
+// Launch School Solution
+// function rotateRightmostDigits(number, count) {
+//   let numberString = String(number); // 735291
+//   let firstPart = numberString.slice(0, numberString.length - count); // 0, 6 - 3 => 0, 3 => indices 0 to 2 => '735' 
+//   let secondPart = numberString.slice(numberString.length - count); // 6 - 3, => 3 => indices 3 to end => 291
+//   let resultString = firstPart + rotateString(secondPart);
+
+//   return Number(resultString);
+// }
+
+// function rotateString(string) { // '291'
+//   return string.slice(1) + string[0]; // '91' + '2'
 // }
 
 // console.log(rotateRightmostDigits(735291, 1));      // 735291
@@ -41,17 +73,53 @@
 
 
 // 3 - Rotation (Part 3)
-// function maxRotation(number) {
-  
-// }
+function maxRotation(number) {
+  let result = '';
+  let string = String(number);
 
-// console.log(maxRotation(735291));          // 321579
-// console.log(maxRotation(3));               // 3
-// console.log(maxRotation(35));              // 53
-// console.log(maxRotation(105));             // 15 -- the leading zero gets dropped
-// console.log(maxRotation(8703529146));      // 7321609845
+  console.log(rotateRightmostDigits(number, string.length));
+
+  for (let i = 0; i < string.length - 1; i++) {
+    rotateRightmostDigits
+
+  } 
+}
 
 
+
+function rotateRightmostDigits(number, count) {
+  let numberString = String(number); // 735291
+  let firstPart = numberString.slice(0, numberString.length - count); // 0, 6 - 3 => 0, 3 => indices 0 to 2 => '735' 
+  let secondPart = numberString.slice(numberString.length - count); // 6 - 3, => 3 => indices 3 to end => 291
+  let resultString = firstPart + rotateString(secondPart);
+
+  return Number(resultString);
+}
+
+function rotateString(string) { // '291'
+  return string.slice(1) + string[0]; // '91' + '2'
+}
+
+
+
+
+
+
+console.log(maxRotation(735291));          // 321579
+console.log(maxRotation(3));               // 3
+console.log(maxRotation(35));              // 53
+console.log(maxRotation(105));             // 15 -- the leading zero gets dropped
+console.log(maxRotation(8703529146));      // 7321609845
+
+// 1st 0 digits fixed, 1st non fixed digit (1st) to the end
+// 735291 // original
+// 352917 // 1st 0 digits fixed, 1st non fixed digit (1st) - 7 to the end
+// 329175 // 1st 1 digits fixed, 1st non fixed digit (2nd) - 5 to the end
+// 321759 // 1st 2 digits fixed, 1st non fixed digit (3rd) - 9 to the end
+// 321597 // 1st 3 digits fixed, 1st non fixed digit (4th) - 7 to the end
+// 321579 // 1st 4 digits fixed, 1st non fixed digit (5th) - 9 to the end
+// 
+// rotations = string.length - 1
 
 
 
