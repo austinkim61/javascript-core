e8633ac4fdb201a9fc0ef1643800e356f6d4592f
Austin Kim <austinkim61@gmail.com>
2025-03-26 22:57:55 -0400

    finished medium 1

--- exercises/js101-js119-small-problems/medium-1/medium-1-exercises.js
+++ exercises/js101-js119-small-problems/medium-1/medium-1-exercises.js
@@ -360,76 +360,87 @@
 
 
 // 5 - Word to Digit
-function wordToDigit(string) {
-  const NUMBERS = {
-    'zero': 0,
-    'one': 1,
-    'two': 2,
-    'three': 3,
-    'four': 4,
-    'five': 5,
-    'six': 6,
-    'seven': 7,
-    'eight': 8,
-    'nine': 9,
-  }
-  return string.split(' ').map(elem => NUMBERS[elem.replace(/[^a-z]/gi, '')] || elem).join(' ');  
-}
+// const NUMBERS = {
+//   zero: 0,
+//   one: 1,
+//   two: 2,
+//   three: 3,
+//   four: 4,
+//   five: 5,
+//   six: 6,
+//   seven: 7,
+//   eight: 8,
+//   nine: 9,
+// };
 
-console.log(wordToDigit('Please call me at five five five one two three four. Thanks.'));
+
+// function wordToDigit(string) {
+//   return string.split(' ').map(elem => NUMBERS[elem.replace(/[^a-z]/gi, '')] || elem).join(' ');  
+// }
+
+// console.log(wordToDigit('Please call me at five five five one two three four. Thanks.'));
 // "Please call me at 5 5 5 1 2 3 4. Thanks."
 
+// function wordToDigit(string) {
+//   return string.replace(/(zero|one|two|three|four|five|six|seven|eight|nine)/gi, match => {
+//     switch (match) {
+//       case 'zero':
+//         return 0;
+//       case 'one':
+//         return 1;
+//       case 'two':
+//         return 2;
+//       case 'three':
+//         return 3;
+//       case 'four':
+//         return 4;
+//       case 'five':
+//         return 5;
+//       case 'six':
+//         return 6;
+//       case 'seven':
+//         return 7;
+//       case 'eight':
+//         return 8;
+//       case 'nine':
+//         return 9;
+//     }
+//   });
+// }
+
+// console.log(wordToDigit('Please call me at five five five one two three four. Thanks.'));
+// // "Please call me at 5 5 5 1 2 3 4. Thanks."
+// console.log(wordToDigit('The weight is done.'));      // "The w8 is d1."
 
 
 
-
-function wordToDigit(string) {
-  let regex = /(one)/;
-
-}
-
-
-const result = str.replace(/(zero)(one)(two)(three)(four)(five)(six)(seven)(eight)(nine))/gi, match => {
-  switch (match) {
-    case 'zero':
-      return 0;
-    case 'one':
-      return 1;
-    case 'two':
-      return 2;
-    case 'three':
-      return 3;
-    case 'four':
-      return 4;
-    case 'five':
-      return 5;
-    case 'six':
-      return 6;
-    case 'seven':
-      return 7;
-    case 'eight':
-      return 8;
-    case 'nine':
-      return 9;
-  }
-});
+// Launch School Solution
+// const NUMBERS = {
+//   zero: 0,
+//   one: 1,
+//   two: 2,
+//   three: 3,
+//   four: 4,
+//   five: 5,
+//   six: 6,
+//   seven: 7,
+//   eight: 8,
+//   nine: 9,
+// };
 
 
+// function wordToDigit(string) {
+//   Object.keys(NUMBERS).forEach(word => {
+//     let regex = new RegExp('\\b' + word + '\\b', 'gi');
+//     string = string.replace(regex, NUMBERS[word]);
+//   });
+//   return string;
+// }
 
-
-console.log(wordToDigit('Please call me at five five five one two three four. Thanks.'));
+// console.log(wordToDigit('Please call me at five five five one two three four. Thanks.'));
 // "Please call me at 5 5 5 1 2 3 4. Thanks."
 
-
-
-
-
-
-
-
-
-
-
+// console.log(wordToDigit('The weight is done.'));      // "The w8 is d1."
 
 
 
@@ -437,16 +448,74 @@ console.log(wordToDigit('Please call me at five five five one two three four. Th
 
 // 6 - Fibonacci Number Location By Length
 // function findFibonacciIndexByLength(integer) {
-  
+//   let first = 1n;
+//   let second = 1n;
+//   let lastDigit = 0n;
+//   let array = [1n, 1n];
+
+//   for (let i = 0; i >= 0; i++) {
+//     lastDigit = first + second;
+//     first = second;
+//     second = lastDigit;
+//     array.push(lastDigit);
+//     if (BigInt(lastDigit.toString().length) === integer) {
+//       break;     
+//     }    
+//   }
+//   return BigInt(array.length);  
 // }
 
-// findFibonacciIndexByLength(2n) === 7n;    // 1 1 2 3 5 8 13
-// findFibonacciIndexByLength(3n) === 12n;   // 1 1 2 3 5 8 13 21 34 55 89 144
-// findFibonacciIndexByLength(10n) === 45n;
-// findFibonacciIndexByLength(16n) === 74n;
-// findFibonacciIndexByLength(100n) === 476n;
-// findFibonacciIndexByLength(1000n) === 4782n;
-// findFibonacciIndexByLength(10000n) === 47847n;
+// console.log(findFibonacciIndexByLength(2n) === 7n);    // 1 1 2 3 5 8 13
+// console.log(findFibonacciIndexByLength(3n) === 12n);   // 1 1 2 3 5 8 13 21 34 55 89 144
+// console.log(findFibonacciIndexByLength(10n) === 45n);
+// console.log(findFibonacciIndexByLength(16n) === 74n);
+// console.log(findFibonacciIndexByLength(100n) === 476n);
+// console.log(findFibonacciIndexByLength(1000n) === 4782n);
+// console.log(findFibonacciIndexByLength(10000n) === 47847n);
+
+// console.log(findFibonacciIndexByLength(2n));
+// console.log(findFibonacciIndexByLength(3n));
+// console.log(findFibonacciIndexByLength(10n));
+// console.log(findFibonacciIndexByLength(16n));
+// console.log(findFibonacciIndexByLength(100n));
+// console.log(findFibonacciIndexByLength(1000n));
+// console.log(findFibonacciIndexByLength(10000n));
+
+
+
+// Launch School Solution
+// function findFibonacciIndexByLength(length) {
+//   let first = 1n;
+//   let second = 1n;
+//   let count = 2n;
+//   let fibonacci;
+
+//   do {
+//     fibonacci = first + second;
+//     count += 1n;
+//     first = second;
+//     second = fibonacci;
+//   } while (String(fibonacci).length < length);
+
+//   return count;
+// }
+
+// console.log(findFibonacciIndexByLength(2n) === 7n);    // 1 1 2 3 5 8 13
+// console.log(findFibonacciIndexByLength(3n) === 12n);   // 1 1 2 3 5 8 13 21 34 55 89 144
+// console.log(findFibonacciIndexByLength(10n) === 45n);
+// console.log(findFibonacciIndexByLength(16n) === 74n);
+// console.log(findFibonacciIndexByLength(100n) === 476n);
+// console.log(findFibonacciIndexByLength(1000n) === 4782n);
+// console.log(findFibonacciIndexByLength(10000n) === 47847n);
+
+
+// console.log(findFibonacciIndexByLength(2n));
+// console.log(findFibonacciIndexByLength(3n));
+// console.log(findFibonacciIndexByLength(10n));
+// console.log(findFibonacciIndexByLength(16n));
+// console.log(findFibonacciIndexByLength(100n));
+// console.log(findFibonacciIndexByLength(1000n));
+// console.log(findFibonacciIndexByLength(10000n));
 
 
 
@@ -454,7 +523,37 @@ console.log(wordToDigit('Please call me at five five five one two three four. Th
 
 // 7 - Fibonacci Numbers (Recursion)
 // function fibonacci(integer) {
-  
+//   return integer === 1 ? 1 : (integer === 0 ? 0 : ((fibonacci(integer - 1) + fibonacci(integer - 2))));  
+// }
+
+// console.log(fibonacci(1));       // 1
+// console.log(fibonacci(2));       // 1
+// console.log(fibonacci(3));       // 2
+// console.log(fibonacci(4));       // 3
+// console.log(fibonacci(5));       // 5
+// console.log(fibonacci(12));      // 144
+// console.log(fibonacci(20));      // 6765
+
+
+// function fibonacci(integer) {
+//   return integer <=2 ? 1 : (fibonacci(integer - 1) + fibonacci(integer - 2));  
+// }
+
+// console.log(fibonacci(1));       // 1
+// console.log(fibonacci(2));       // 1
+// console.log(fibonacci(3));       // 2
+// console.log(fibonacci(4));       // 3
+// console.log(fibonacci(5));       // 5
+// console.log(fibonacci(12));      // 144
+// console.log(fibonacci(20));      // 6765
+
+
+// Launch School Solution
+// function fibonacci(nth) {
+//   if (nth <= 2) {
+//     return 1;
+//   }
+//   return fibonacci(nth - 1) + fibonacci(nth - 2);
 // }
 
 // console.log(fibonacci(1));       // 1
@@ -471,7 +570,37 @@ console.log(wordToDigit('Please call me at five five five one two three four. Th
 
 // 8 - Fibonacci Numbers (Procedural)
 // function fibonacci(integer) {
-  
+//   let first = 1;
+//   let second = 1;
+//   let lastDigit;
+
+//   if (integer < 3) {
+//     return 1;
+//   }
+
+//   for (let i = 3; i <= integer; i++) {
+//     lastDigit = first + second;
+//     first = second;
+//     second = lastDigit;
+//   }
+//   return lastDigit;
+// }
+
+// console.log(fibonacci(20));       // 6765
+// console.log(fibonacci(50));       // 12586269025
+// console.log(fibonacci(75));       // 2111485077978050
+
+
+
+// Launch School Solution
+// function fibonacci(nth) {
+//   let previousTwo = [1, 1];
+
+//   for (let counter = 3; counter <= nth; counter += 1) {
+//     previousTwo = [previousTwo[1], previousTwo[0] + previousTwo[1]];
+//   }
+
+//   return previousTwo[1];
 // }
 
 // console.log(fibonacci(20));       // 6765
@@ -483,35 +612,34 @@ console.log(wordToDigit('Please call me at five five five one two three four. Th
 
 
 // 9 - Fibonacci Numbers (Memoization)
-// function fibonacci(integer) {
-  
+// let obj = {};
+// function fibonacci(n) {
+//   if (n in obj) { // calls on it if in object
+//     return obj[n];
+//   }
+//   if (n <= 2) { // lowest level calls
+//     obj[n] = 1;
+//   } else { // recursion but starts to store in object
+//     obj[n] = fibonacci(n - 1) + fibonacci(n - 2);
+//   }
+//   return obj[n];
 // }
 
-// console.log(fibonacci(20));       // 6765
-// console.log(fibonacci(50));       // 12586269025
 // console.log(fibonacci(75));       // 2111485077978050
 
 
 
+// Launch School Solution
+// let memo = {};
+// function fibonacci(n) {
+//   if (n <= 2) {
+//     return 1;
+//   } else if (n in memo) {
+//     return memo[n];
+//   } else {
+//     memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+//   }
+//   return fibonacci(n);
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// console.log(fibonacci(75));       // 2111485077978050
