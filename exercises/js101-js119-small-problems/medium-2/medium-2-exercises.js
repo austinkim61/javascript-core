c89c93cb985ec69f390de01ea43c2703b1057e42
Austin Kim <austinkim61@gmail.com>
2025-03-26 23:06:22 -0400

    readied medium 2

--- exercises/js101-js119-small-problems/medium-2/medium-2-exercises.js
+++ exercises/js101-js119-small-problems/medium-2/medium-2-exercises.js
@@ -1,17 +1,137 @@
-// 1 - 
-// 2 - 
-// 3 - 
-// 4 - 
-// 5 - 
-// 6 - 
-// 7 - 
-// 8 - 
-// 9 - 
-// 10 - 
-// 11 - 
-// 12 - 
+// 1 - Lettercase Percentage Ratio
+function letterPercentages(string) {
+
+}
+
+console.log(letterPercentages('abCdef 123'));
+// { lowercase: "50.00", uppercase: "10.00", neither: "40.00" }
+
+console.log(letterPercentages('AbCd +Ef'));
+// { lowercase: "37.50", uppercase: "37.50", neither: "25.00" }
+
+console.log(letterPercentages('123'));
+// { lowercase: "0.00", uppercase: "0.00", neither: "100.00" }
 
 
+
+
+
+// 2 - Triangle Sides
+// function triangle(num1, num2, num3) {
+
+// }
+
+// console.log(triangle(3, 3, 3));        // "equilateral"
+// console.log(triangle(3, 3, 1.5));      // "isosceles"
+// console.log(triangle(3, 4, 5));        // "scalene"
+// console.log(triangle(0, 3, 3));        // "invalid"
+// console.log(triangle(3, 1, 1));        // "invalid"
+
+
+
+
+
+// 3 - Tri-Angles
+// function triangle(num1, num2, num3) {
+
+// }
+
+// console.log(triangle(60, 70, 50));       // "acute"
+// console.log(triangle(30, 90, 60));       // "right"
+// console.log(triangle(120, 50, 10));      // "obtuse"
+// console.log(triangle(0, 90, 90));        // "invalid"
+// console.log(triangle(50, 50, 50));       // "invalid"
+
+
+
+
+
+// 4 - Unlucky Days
+// function fridayThe13ths(number) {
+
+// }
+
+// console.log(fridayThe13ths(1986));      // 1
+// console.log(fridayThe13ths(2015));      // 3
+// console.log(fridayThe13ths(2017));      // 2
+
+
+
+
+
+// 5 - Next Featured Number Higher than a Given Value
+// function featured(number) {
+
+// }
+
+// console.log(featured(12));           // 21
+// console.log(featured(20));           // 21
+// console.log(featured(21));           // 35
+// console.log(featured(997));          // 1029
+// console.log(featured(1029));         // 1043
+// console.log(featured(999999));       // 1023547
+// console.log(featured(999999987));    // 1023456987
+// console.log(featured(9876543186));   // 9876543201
+// console.log(featured(9876543200));   // 9876543201
+// console.log(featured(9876543201));   // "There is no possible number that fulfills those requirements."
+
+
+// 6 - Sum Square - Square Sum
+// function sumSquareDifference(number) {
+
+// }
+
+// console.log(sumSquareDifference(3));      // 22 --> (1 + 2 + 3)**2 - (1**2 + 2**2 + 3**2)
+// console.log(sumSquareDifference(10));     // 2640
+// console.log(sumSquareDifference(1));      // 0
+// console.log(sumSquareDifference(100));    // 25164150
+
+
+
+
+
+// 7 - Bubble Sort
+// function bubbleSort(array) {
+
+// }
+
+// let array1 = [5, 3];
+// bubbleSort(array1);
+// console.log(array1);    // [3, 5]
+
+// let array2 = [6, 2, 7, 1, 4];
+// bubbleSort(array2);
+// console.log(array2);    // [1, 2, 4, 6, 7]
+
+// let array3 = ['Sue', 'Pete', 'Alice', 'Tyler', 'Rachel', 'Kim', 'Bonnie'];
+// bubbleSort(array3);
+// console.log(array3);    // ["Alice", "Bonnie", "Kim", "Pete", "Rachel", "Sue", "Tyler"]
+
+
+
+
+
+// 8 - Is It Prime?
+// function isPrime(number) {
+
+// }
+
+// console.log(isPrime(1) === false)            // true
+// console.log(isPrime(2) === true)             // true
+// console.log(isPrime(3) === true)             // true
+// console.log(isPrime(4) === false)            // true
+// console.log(isPrime(5) === true)             // true
+// console.log(isPrime(6) === false)            // true
+// console.log(isPrime(7) === true)             // true
+// console.log(isPrime(8) === false)            // true
+// console.log(isPrime(9) === false)            // true
+// console.log(isPrime(10) === false)           // true
+// console.log(isPrime(23) === true)            // true
+// console.log(isPrime(24) === false)           // true
+// console.log(isPrime(997) === true)           // true
+// console.log(isPrime(998) === false)          // true
+// console.log(isPrime(3_297_061) === true)     // true
+// console.log(isPrime(23_297_061) === false)   // true
 
 
 
