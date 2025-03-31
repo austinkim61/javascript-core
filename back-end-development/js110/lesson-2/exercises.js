f3c4b45426ae54e065aa2b2f4a82b05c9e6f907d
Austin Kim <austinkim61@gmail.com>
2025-03-30 23:15:36 -0400

    started next lesson

---
+++ back-end-development/js110/lesson-2/exercises.js
@@ -0,0 +1,75 @@
+// let words = ['go', 'ahead', 'and', 'jump'];
+// words.sort((a, b) => a.length - b.length);
+// console.log(words);
+
+// let words = ['go', 'ahead', 'and', 'jump'];
+// words.sort((a, b) => {
+//   if (a.length < b.length) {
+//     return -1;
+//   } else if (a.length > b.length) {
+//     return 1;
+//   } else {
+//     return 0
+//   }
+// });
+// console.log(words);
+
+
+
+
+
+// let scores = [[3, 6, 4], [6, 8, 9], [1, 4, 2]];
+// scores.sort((a, b) => a.reduce((acc, elem) => acc + elem, 0) - b.reduce((acc, elem) => acc + elem, 0));
+// console.log(scores);
+
+// let scores = [[3, 6, 4], [6, 8, 9], [1, 4, 2]];
+// scores.sort((a, b) => {
+//   let totalAScore = a.reduce((acc, elem) => acc + elem);
+//   let totalBScore = b.reduce((acc, elem) => acc + elem);
+//   return totalAScore - totalBScore;
+// });
+// console.log(scores);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
