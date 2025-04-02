03b325877d5215a57ee87a95298e3777aeecb570
Austin Kim <austinkim61@gmail.com>
2025-04-01 23:36:13 -0400

    updated tracking

--- back-end-development/js110/lesson-2/practice-problems.js
+++ back-end-development/js110/lesson-2/practice-problems.js
@@ -1,8 +1,34 @@
 // Question 1
+// let arr = ['10', '11', '9', '7', '8'];
+
+// console.log(arr.map(string => Number(string)).sort((a, b) => {
+//   if (a > b) {
+//     return -1;
+//   } else if (a < b) {
+//     return 1;
+//   } else {
+//     return 0;
+//   }
+// }));
+
+// console.log(arr.map(string => Number(string)).sort((a, b) => b - a));
+
+// Launch School Solution
+// console.log(arr.sort((a, b) => Number(b) - Number(a)));
+
+
 
 
 
 // Question 2
+let books = [
+  { title: 'One Hundred Years of Solitude', author: 'Gabriel Garcia Marquez', published: '1967' },
+  { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', published: '1925' },
+  { title: 'War and Peace', author: 'Leo Tolstoy', published: '1869' },
+  { title: 'Ulysses', author: 'James Joyce', published: '1922' },
+  { title: 'The Book of Kells', author: 'Multiple Authors', published: '800' },
+];
+
 
 
 
