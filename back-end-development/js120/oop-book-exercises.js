cd01391c27404f083b0b46483146c86efed28a9c
Austin Kim <austinkim61@gmail.com>
2025-04-30 18:53:34 -0400

    started oop book

---
+++ back-end-development/js120/oop-book-exercises.js
@@ -0,0 +1,137 @@
+// Types and Objects Exercises
+// Exercise 1
+// let cessna152 = {
+//   fuelCapacityInGallons: 24.5, // state
+//   cruisingSpeedInKnots: 111, // state
+
+//   takeOff() {
+//     console.log('Taking off!'); // behavior
+//   },
+
+//   land() {
+//     console.log('Landing!'); // behavior
+//   },
+// };
+
+// Alternate
+// let cessna152 = {
+//   fuelCapacityInGallons: 24.5, // state
+//   cruisingSpeedInKnots: 111, // state
+
+//   takeOff: function() {
+//     console.log('Taking off!'); // behavior
+//   },
+
+//   land: function() {
+//     console.log('Landing!'); // behavior
+//   },
+// };
+
+// cessna152.takeOff();
+// cessna152.land();
+
+
+
+// Exercise 2
+// function Book(title, author, published) {
+//   this.title = title;
+//   this.author = author;
+//   this.published = published;
+// }
+
+// let neuromancer = new Book('Neuromancer', 'William Gibson', 1984);
+// let doomsdayBook = new Book('Doomsday Book', 'Connie Willis', 1992);
+
+// console.log(neuromancer);
+// console.log(doomsdayBook);
+// object type is Book
+// constructor is the Book function
+// instances objects are the objects assigned to neuromancer and doomsdayBook
+
+
+
+// Exercise 3
+// function Album(title, artist, year) {
+//   this.title = title;
+//   this.artist = artist;
+//   this.year = year;
+// }
+
+// let thriller = new Album('Thriller', 'Michael Jackson', 1982);
+// let theDarkSideOfTheMoon = new Album('The Dark Side of the Moon', 'Pink Floyd', 1973);
+
+// console.log(thriller);
+// console.log(theDarkSideOfTheMoon);
+// object type is Album
+// constructor is the Album function
+// instance objects are the objects assigned to thriller and darkSideOfTheMoon
+
+
+
+// Exercise 4
+// function Smartphone(brand, model, releaseYear) {
+//   this.brand = brand;
+//   this.model = model;
+//   this.releaseYear = releaseYear;
+
+//   this.battery = function() {
+//     return `${this.brand} ${this.model} has 69% battery remaining.`;
+//   };
+
+//   this.displayInfo = function() {
+//     return `${this.releaseYear} ${this.brand} ${this.model}`;
+//   };
+// }
+
+// let apple = new Smartphone('Apple', 'iPhone 12', 2020);
+// let samsung = new Smartphone('Samsung', 'Galaxy S21', 2021);
+
+// console.log(apple.battery());
+// console.log(apple.displayInfo());
+// console.log(samsung.battery());
+// console.log(samsung.displayInfo());
+
+
+
+
+
+// The Object Model Exercises
+
+
+
+
+
+
+// Object Factories Exercises
+
+
+
+
+
+
+// Classes Exercises
+
+
+
+
+
+
+// More About Classes Exercises
+
+
+
+
+
+
+// Protptypal Inheritance Exercises
+
+
+
+
+
+
+
+
+
+
+
