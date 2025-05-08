46081eaa0702a222bc214044e6dfb7a0b45c5d21
Austin Kim <austinkim61@gmail.com>
2025-05-07 22:51:17 -0400

    done "more about classes"

--- back-end-development/js120/exercises.js
+++ back-end-development/js120/exercises.js
@@ -1,35 +1,57 @@
-class Student {
-  constructor(firstName, lastName, track) {
-    this._firstName = firstName;
-    this._lastName = lastName;
-    this._track = track;
+// class Student {
+//   constructor(firstName, lastName, track) {
+//     this._firstName = firstName;
+//     this._lastName = lastName;
+//     this._track = track;
+//   }
+
+//   get name() {
+//     return [this.firstName, this.lastName];
+//   }
+
+//   get firstName() {
+//     return this._firstName;
+//   }
+
+//   get lastName() {
+//     return this._lastName;
+//   }
+
+//   get track() {
+//     return this._track;
+//   }
+// }
+
+// let student = new Student('Kay', 'Oakley', 'JavaScript');
+// console.log(`${student.name.join(' ')} ${student.track}`);
+// // Kay Oakley JavaScript
+// console.log(`${student.firstName} ${student.lastName}`);
+// // Kay Oakley
+
+// student.firstName = 'hello';
+
+// console.log(student);
+
+
+
+
+
+
+class MyClass {
+  static staticMethod() {
+    console.log('This is a static method.');
   }
 
-  get name() {
-    return [this.firstName, this.lastName];
-  }
-
-  get firstName() {
-    return this._firstName;
-  }
-
-  get lastName() {
-    return this._lastName;
-  }
-
-  get track() {
-    return this._track;
+  instanceMethod() {
+    console.log('This is an instance method.');
   }
 }
 
-let student = new Student('Kay', 'Oakley', 'JavaScript');
-console.log(`${student.name.join(' ')} ${student.track}`);
-// Kay Oakley JavaScript
-console.log(`${student.firstName} ${student.lastName}`);
-// Kay Oakley
+// Calling the static method
+MyClass.staticMethod();    // This is a static method.
+MyClass.instanceMethod();
 
-student.firstName = 'hello';
+// const instance = new MyClass();
 
-console.log(student);
-
-
+// instance.instanceMethod(); // This is an instance method.
+// instance.staticMethod();   // Raises error
--- back-end-development/js120/oop-book-exercises.js
+++ back-end-development/js120/oop-book-exercises.js
@@ -370,30 +370,236 @@ Which of the concepts discussed in this chapter most closely describes the featu
 
 // More About Classes Exercises
 // Exercise 1
+// class Person {
+//   #name;
+//   #age;
 
+//   constructor(name, age) {
+//     this.#name = name;
+//     this.age = age;
+//   }
+
+//   get name() {
+//     return this.#name;
+//   }
+
+//   get age() {
+//     return this.#age;
+//   }
+
+//   showName() {
+//     console.log(this.#name);
+//   }
+
+//   showAge() {
+//     console.log(this.#age);
+//   }
+
+//   set age(age) {
+//     if (typeof(age) === 'number' && age > 0) {
+//       this.#age = age;
+//     } else {
+//       throw new RangeError('Age must be positive');
+//     }
+//   }
+// }
+
+// let person = new Person('John', 30);
+// person.showAge(); // 30
+// person.age = 31;
+// person.showAge(); // 31
+
+// try {
+//   // This line should raise a RangeError,
+//   // but does not.
+//   person.age = -5;
+//   person.showAge(); // -5
+// } catch (e) {
+//   // The following line should run, but won't
+//   console.log('RangeError: Age must be positive');
+// }
 
 
 
 // Exercise 2
+// class Book {
+//   #title;
+//   #author;
+//   #year;
 
+//   constructor(title, author, year) {
+//     this.#title = title;
+//     this.#author = author;
+//     this.year = year;
+//   }
+
+//   get title() {
+//     return this.#title;
+//   }
+
+//   get author() {
+//     return this.#author;
+//   }
+
+//   get year() {
+//     return this.#year;
+//   }
+
+//   set year(year) {
+//     if (typeof(year) === 'number' && year >= 1900) {
+//       this.#year = year;
+//     } else {
+//       throw new RangeError('Invalid year');
+//     }
+//   }
+// }
+
+
+// let book = new Book('The Great Gatsby', 'F. Scott Fitzgerald', 1925);
+// console.log(book.title);  // The Great Gatsby
+// console.log(book.author); // F. Scott Fitzgerald
+// console.log(book.year);   // 1925
+
+// book.year = 1932;         // Changing year
+// console.log(book.year);   // 1932
+
+
+// try {
+//   book.year = 1825;
+// } catch (e) {
+//   console.log(e);   // RangeError: Invalid year
+// }
+
+// try {
+//   let book2 = new Book('A Tale of Two Cities', 'Charles Dickents', 1859);
+// } catch (e) {
+//   console.log(e);   // RangeError: Invalid year
+// }
 
 
 
 // Exercise 3
+// class BankAccount{
+//   #balance = 0;
 
+//   #checkBalance() {
+//     console.log(`Current balance: $${this.#balance}`);
+//   }
+
+//   deposit(amount) {
+//     this.#balance += amount;
+//     this.#checkBalance();
+//   }
+
+//   withdraw(amount) {
+//     if (this.#balance >= amount) {
+//       this.#balance -= amount;
+//       this.#checkBalance();
+//     } else {
+//       throw new RangeError('Insufficient funds');
+//     }
+//   }
+// }
+
+// let account = new BankAccount();
+// account.deposit(100);
+// account.withdraw(50);
+// account.withdraw(100); // RangeError: Insufficient funds
 
 
 
 // Exercise 4
+// class Rectangle {
+//   #width;
+//   #height;
 
+//   constructor(width, height) {
+//     this.width = width;
+//     this.height = height;
+//   }
+
+//   get width() {
+//     return this.#width;
+//   }
+
+//   get height() {
+//     return this.#height;
+//   }
+
+//   get area() {
+//     return this.width * this.height;
+//   }
+
+//   set width(width) {
+//     if (typeof (width) === 'number' && width > 0) {
+//       this.#width = width;
+//     } else {
+//       throw new RangeError('width must be positive');
+//     }
+//   }
+
+//   set height(height) {
+//     if (typeof (height) === 'number' && height > 0) {
+//       this.#height = height;
+//     } else {
+//       throw new RangeError('height must be positive');
+//     }
+//   }
+// }
+
+// let rect = new Rectangle(10, 5);
+// console.log(rect.area);
+
+// rect.width = 20;
+// console.log(rect.area);
+
+// rect.height = 12;
+// console.log(rect.area);
+
+// try {
+//   rect.width = 0;
+// } catch (e) {
+//   console.log(e);
+// }
+
+// try {
+//   rect.height = -10;
+// } catch (e) {
+//   console.log(e);
+// }
 
 
 
 // Exercise 5
+class MathUtils {
+
+  static add(num1, num2) {
+    return num1 + num2;
+  }
+
+  static subtract(num1, num2) {
+    return num1 - num2;
+  }
+
+  static multiply(num1, num2) {
+    return num1 * num2;
+  }
+
+  static divide(num1, num2) {
+    if (num2 !== 0) {
+      return num1 / num2;
+    } else {
+      throw new RangeError('Division by zero');
+    }
+  }
+}
 
 
-
-
+console.log(MathUtils.add(5, 3));
+console.log(MathUtils.subtract(10, 4));
+console.log(MathUtils.multiply(6, 7));
+console.log(MathUtils.divide(20, 5));
+console.log(MathUtils.divide(10, 0));
 
 
 
@@ -401,6 +607,10 @@ Which of the concepts discussed in this chapter most closely describes the featu
 
 // Protptypal Inheritance Exercises
 // Exercise 1
+
+
+
+
 
 
 
