2f09808576ae55fc13f8575daddb4b2ef75b85d4
Austin Kim <austinkim61@gmail.com>
2025-05-18 20:54:07 -0400

    getting ready for practice problems

--- back-end-development/js120/lesson-2/exercises.js
+++ back-end-development/js120/lesson-2/exercises.js
@@ -14,9 +14,35 @@
 // console.log(myObject['[object Object]']);
 
 
-let myObject = { };
-myObject["akey"] = 'four';
-console.log(myObject.akey);
-console.log(myObject["akey"]);
+// let myObject = { };
+// myObject["akey"] = 'four';
+// console.log(myObject.akey);
+// console.log(myObject["akey"]);
+
+
+let arr = [1, 2, 3];
+console.log(arr.propertyIsEnumerable('length'));                     // false
+console.log(arr.propertyIsEnumerable('2'));                          // true
+console.log(arr.propertyIsEnumerable('forEach'));                    // false
+console.log(Array.prototype.propertyIsEnumerable('forEach'));        // false
+
+function Foo() {
+  this.bar = "qux"
+}
+
+Foo.prototype.baz = function() {};
+let foo = new Foo();
+console.log(foo.propertyIsEnumerable('bar'));                        // true
+console.log(Object.getPrototypeOf(foo).propertyIsEnumerable('baz')); // true
+console.log(foo.propertyIsEnumerable('baz')); // false
+
+
+
+
+
+
+
+
+
 
 
