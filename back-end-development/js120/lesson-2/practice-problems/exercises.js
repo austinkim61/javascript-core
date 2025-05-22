5bea73b7170d23e9947b0b03833dcc3a4a593df9
Austin Kim <austinkim61@gmail.com>
2025-05-21 23:48:01 -0400

    finished lessons up to "the global object"

--- back-end-development/js120/lesson-2/practice-problems/exercises.js
+++ back-end-development/js120/lesson-2/practice-problems/exercises.js
@@ -1,9 +1,25 @@
-bar();
-function bar() {
-  console.log("this is bar");
-}
+// bar();
+// function bar() {
+//   console.log("this is bar");
+// }
 
-foo();
-const foo = function() {
-  console.log("this is foo");
-};
+// foo();
+// const foo = function() {
+//   console.log("this is foo");
+// };
+
+
+
+// let a = (x) => x * x;
+// let a = () => console.log('hello');
+// a();
+
+
+
+
+
+
+
+
+
+
