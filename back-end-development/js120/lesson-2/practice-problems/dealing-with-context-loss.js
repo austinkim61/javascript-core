ab373c7b7ab0e43452dd61562358bec0d19efa9c
Austin Kim <austinkim61@gmail.com>
2025-05-26 22:52:25 -0400

    prepare for lesson 2 quizzes 1 and 2 next

--- back-end-development/js120/lesson-2/practice-problems/dealing-with-context-loss.js
+++ back-end-development/js120/lesson-2/practice-problems/dealing-with-context-loss.js
@@ -1,55 +1,264 @@
-// Problem
+// Problem 1
+// let turk = {
+//   firstName: 'Christopher',
+//   lastName: 'Turk',
+//   occupation: 'Surgeon',
+//   getDescription() {
+//       return this.firstName + ' ' + this.lastName + ' is a '
+//                                   + this.occupation + '.';
+//   }
+// };
+
+// function logReturnVal(func) {
+//   let returnVal = func();
+//   console.log(returnVal);
+// }
+
+// logReturnVal(turk.getDescription);
+
+// // answer => logs 'undefined undefined is a undefined.
+// //        => when turk.getDescription is passed to logReturnValue as an arguemtn, the method is removed from its context
+// //        => when func is executed, this points to the global object rather than turk
 
 
 
 
 
+// Problem 2
+// let turk = {
+//   firstName: 'Christopher',
+//   lastName: 'Turk',
+//   occupation: 'Surgeon',
+//   getDescription() {
+//       return this.firstName + ' ' + this.lastName + ' is a '
+//                                   + this.occupation + '.';
+//   }
+// };
 
+// function logReturnVal(func, context) {
+//   let returnVal = func.call(context);
+//   console.log(returnVal);
+// }
 
-// Problem
-
-
-
-
-
-
-
-// Problem
-
-
-
-
-
-
-
-// Problem
-
+// logReturnVal(turk.getDescription, turk);
 
 
 
 
 
+// Problem 3
+// let turk = {
+//   firstName: 'Christopher',
+//   lastName: 'Turk',
+//   occupation: 'Surgeon',
+//   getDescription() {
+//       return this.firstName + ' ' + this.lastName + ' is a '
+//                                   + this.occupation + '.';
+//   }
+// };
 
-// Problem
+// function logReturnVal(func) {
+//   let returnVal = func();
+//   console.log(returnVal);
+// }
+
+// let turkDescription = turk.getDescription.bind(turk);
+// logReturnVal(turkDescription);
 
 
 
 
 
+// Problem 4
+// const TESgames = {
+//   titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+//   seriesTitle: 'The Elder Scrolls',
+//   listGames: function() {
+//     this.titles.forEach(function(title) {
+//       console.log(this.seriesTitle + ': ' + title);
+//     });
+//   }
+// };
+
+// TESgames.listGames();
+
+// // answer => will not output the below because forEach executes the function expression passed to it so it gets executed with the global object as its context
+// //        => does log the title because the array it is iterating over is this.titles and calling it with TESgames as the object allows it to access this
+// /*
+// The Elder Scrolls: Arena
+// The Elder Scrolls: Daggerfall
+// The Elder Scrolls: Morrowind
+// The Elder Scrolls: Oblivion
+// The Elder Scrolls: Skyrim
+// */
 
 
-// Problem
+
+
+
+// Problem 5
+// const TESgames = {
+//   titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+//   seriesTitle: 'The Elder Scrolls',
+//   listGames: function() {
+//     let self = this;
+//     this.titles.forEach(function(title) {
+//       console.log(self.seriesTitle + ': ' + title);
+//     });
+//   }
+// };
+
+// TESgames.listGames();
 
 
 
 
 
+// Problem 6
+// const TESgames = {
+//   titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+//   seriesTitle: 'The Elder Scrolls',
+//   listGames: function() {
+//     this.titles.forEach(function(title) {
+//       console.log(this.seriesTitle + ': ' + title);
+//     }, this);
+//   }
+// };
+
+// TESgames.listGames();
 
 
 
 
 
+// Problem 7
+// const TESgames = {
+//   titles: ['Arena', 'Daggerfall', 'Morrowind', 'Oblivion', 'Skyrim'],
+//   seriesTitle: 'The Elder Scrolls',
+//   listGames: function() {
+//     this.titles.forEach(title => {
+//       console.log(this.seriesTitle + ': ' + title);
+//     });
+//   }
+// };
 
+// TESgames.listGames();
+
+
+
+
+
+// Problem 8
+// let foo = {
+//   a: 0,
+//   incrementA: function() {
+//     function increment() {
+//       this.a += 1;
+//     }
+
+//     increment();
+//   }
+// };
+
+// foo.incrementA();
+// foo.incrementA();
+// foo.incrementA();
+// console.log(foo.a);
+
+// // answer => 0
+
+
+
+
+
+// Problem 9
+// let foo = {
+//   a: 0,
+//   incrementA: function() {
+//     let self = this;
+//     function increment() {
+//       self.a += 1;
+//     }
+
+//     increment();
+//   }
+// };
+
+// foo.incrementA();
+// foo.incrementA();
+// foo.incrementA();
+// console.log(foo.a);
+
+
+
+// let foo = {
+//   a: 0,
+//   incrementA: function() {
+//     function increment() {
+//       this.a += 1;
+//     }
+
+//     increment.call(this);
+//   }
+// };
+
+// foo.incrementA();
+// foo.incrementA();
+// foo.incrementA();
+// console.log(foo.a);
+
+
+
+// let foo = {
+//   a: 0,
+//   incrementA: function() {
+//     let increment = function() {
+//       this.a += 1;
+//     }.bind(this);
+
+//     increment();
+//   }
+// };
+
+// foo.incrementA();
+// foo.incrementA();
+// foo.incrementA();
+// console.log(foo.a);
+
+
+
+// let foo = {
+//   a: 0,
+//   incrementA: function() {
+//     let increment = () => {
+//       this.a += 1;
+//     }
+//     increment();
+//   }
+// };
+
+// foo.incrementA();
+// foo.incrementA();
+// foo.incrementA();
+// console.log(foo.a);
+
+
+
+// let foo = {
+//   a: 0,
+//   incrementA: function() {
+//     function increment() {
+//       this.a += 1;
+//     }
+
+//     increment.apply(this);
+//   }
+// };
+
+// foo.incrementA();
+// foo.incrementA();
+// foo.incrementA();
+// console.log(foo.a);
 
 
 
