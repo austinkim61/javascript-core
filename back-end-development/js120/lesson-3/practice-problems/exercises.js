8584d70bf24fcc8d761068e1ee5e59f1fe3bf9a6
Austin Kim <austinkim61@gmail.com>
2025-06-01 17:08:03 -0400

    starting constructors and prototypes practice problems

--- back-end-development/js120/lesson-3/practice-problems/exercises.js
+++ back-end-development/js120/lesson-3/practice-problems/exercises.js
@@ -3,29 +3,37 @@
 
 
 
-function Lizard() {
-  this.scamper = function() {
-    console.log("I'm scampering!");
-  };
+// function Lizard() {
+//   this.scamper = function() {
+//     console.log("I'm scampering!");
+//   };
+// }
+
+// let lizzy = new Lizard();
+// lizzy.scamper(); // ?
+
+
+
+
+
+
+
+function Dog(name, breed, weight) {
+  this.name = name;
+  this.breed = breed;
+  this.weight = weight;
 }
 
-let lizzy = new Lizard();
-lizzy.scamper(); // ?
+Dog.prototype.bark = function() {
+  console.log(this.weight > 20 ? 'Woof!' : 'Yip!');
+};
 
+let maxi = new Dog('Maxi', 'German Shepherd', 32);
 
+Dog.prototype.constructor = function() {};
 
-
-
-
-
-
-
-
-
-
-
-
-
+maxi.constructor === Dog; // false
+maxi instanceof Dog;      // true
 
 
 
