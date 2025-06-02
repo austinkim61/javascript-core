8537e95a9a38543c7b1450e4ca0957f5d1efa826
Austin Kim <austinkim61@gmail.com>
2025-06-01 22:37:45 -0400

    started constructors and prototypes exercises

--- back-end-development/js120/lesson-3/practice-problems/constructors-and-prototypes.js
+++ back-end-development/js120/lesson-3/practice-problems/constructors-and-prototypes.js
@@ -1,60 +1,174 @@
 // Problem 1
+// let RECTANGLE = {
+//   area: function() {
+//     return this.width * this.height;
+//   },
+//   perimeter: function() {
+//     return 2 * (this.width + this.height);
+//   },
+// };
 
+// function Rectangle(width, height) {
+//   this.width = width;
+//   this.height = height;
+//   this.area = RECTANGLE.area();
+//   this.perimeter = RECTANGLE.perimeter();
+// }
 
+// let rect1 = new Rectangle(2, 3);
 
+// console.log(rect1.area);
+// console.log(rect1.perimeter);
 
-
+// // NaN
+// // NaN
 
 
 
 
 
 // Problem 2
+// let RECTANGLE = {
+//   area: function() {
+//     return this.width * this.height;
+//   },
+//   perimeter: function() {
+//     return 2 * (this.width + this.height);
+//   },
+// };
 
+// function Rectangle(width, height) {
+//   this.width = width;
+//   this.height = height;
+//   this.area = RECTANGLE.area.call(this);
+//   this.perimeter = RECTANGLE.perimeter.call(this);
+// }
 
+// let rect1 = new Rectangle(2, 3);
 
-
-
+// console.log(rect1.area);
+// console.log(rect1.perimeter);
 
 
 
 
 
 // Problem 3
+// function Circle(radius) {
+//   this.radius = radius;
+// }
+
+// Circle.prototype.area = function() {
+//   return Number(Math.PI * Math.pow(this.radius, 2));
+// };
+
+// let a = new Circle(3);
+// let b = new Circle(4);
+
+// console.log(a.area().toFixed(2)); // => 28.27
+// console.log(b.area().toFixed(2)); // => 50.27
+// console.log(a.hasOwnProperty('area')); // => false
 
 
 
+// Launch School Solution
+// const Circle = function(radius) {
+//   this.radius = radius;
+// }
 
+// Circle.prototype.area = function() {
+//   return Math.PI * this.radius * this.radius;
+// };
 
+// let a = new Circle(3);
+// let b = new Circle(4);
+
+// console.log(a.area().toFixed(2)); // => 28.27
+// console.log(b.area().toFixed(2)); // => 50.27
+// console.log(a.hasOwnProperty('area')); // => false
 
 
 
 
 
 // Problem 4
+// function Ninja() {
+//   this.swung = true;
+// }
 
+// let ninja = new Ninja();
 
+// Ninja.prototype.swingSword = function() {
+//   return this.swung;
+// };
 
+// console.log(ninja.swingSword());
 
-
+// // true
 
 
 
 
 
 // Problem 5
+// function Ninja() {
+//   this.swung = true;
+// }
 
+// let ninja = new Ninja();
 
+// Ninja.prototype = {
+//   swingSword: function() {
+//     return this.swung;
+//   },
+// };
 
+// console.log(ninja.swingSword());
 
-
+// // "TypeError: ninja.swingSword is not a function" because the prototype was changed after ninja was created
 
 
 
 
 
 // Problem 6
+function Ninja() {
+  Object.setPrototypeOf(this, Ninja.myPrototype);
+  this.swung = false;
+}
 
+Ninja.myPrototype = {
+
+  swing() {
+    return this.swung = false;
+
+  },
+}
+
+
+
+// Ninja.prototype.swing = function() {
+//   // this.swung = true;
+//   return this.swung = true;
+// }
+
+// Ninja.prototype = {
+//   swung: false,
+//   swing: function() {
+//     return this.swung = true;
+//   },
+
+// };
+
+
+// Add a swing method to the Ninja prototype which
+// modifies `swung` and returns the calling object
+
+let ninjaA = new Ninja();
+let ninjaB = new Ninja();
+
+console.log(ninjaA.swing().swung);      // logs `true`
+console.log(ninjaB.swing().swung);      // logs `true`
 
 
 
@@ -76,130 +190,6 @@
 
 
 // Problem 8
-
-
-
-
-
-
-
-
-
-
-// Problem 9
-
-
-
-
-
-
-
-
-
-
-// Problem 10
-
-
-
-
-
-
-
-
-
-
-// Problem 11
-
-
-
-
-
-
-
-
-
-
-// Problem 12
-
-
-
-
-
-
-
-
-
-
-// Problem 13
-
-
-
-
-
-
-
-
-
-
-// Problem 14
-
-
-
-
-
-
-
-
-
-
-// Problem 15
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- back-end-development/js120/lesson-3/practice-problems/exercises.js
+++ back-end-development/js120/lesson-3/practice-problems/exercises.js
@@ -24,34 +24,21 @@ function Dog(name, breed, weight) {
   this.weight = weight;
 }
 
-Dog.prototype.bark = function() {
-  console.log(this.weight > 20 ? 'Woof!' : 'Yip!');
-};
-
-let maxi = new Dog('Maxi', 'German Shepherd', 32);
-
-Dog.prototype.constructor = function() {};
-
-maxi.constructor === Dog; // false
-maxi instanceof Dog;      // true
+Dog.prototype
 
 
+console.log(Dog.prototype);
 
+// Dog.prototype.bark = function() {
+//   console.log(this.weight > 20 ? 'Woof!' : 'Yip!');
+// };
 
+// let maxi = new Dog('Maxi', 'German Shepherd', 32);
 
+// Dog.prototype.constructor = function() {};
 
-
-
-
-
-
-
-
-
-
-
-
-
+// maxi.constructor === Dog; // false
+// maxi instanceof Dog;      // true
 
 
 
