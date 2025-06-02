e7b71cfbde7a824f5f7b4e6348c5a215e885a397
Austin Kim <austinkim61@gmail.com>
2025-06-02 18:39:36 -0400

    finished constructors and prototypes problems

--- back-end-development/js120/lesson-3/practice-problems/constructors-and-prototypes.js
+++ back-end-development/js120/lesson-3/practice-problems/constructors-and-prototypes.js
@@ -132,239 +132,57 @@
 
 
 // Problem 6
-function Ninja() {
-  Object.setPrototypeOf(this, Ninja.myPrototype);
-  this.swung = false;
-}
-
-Ninja.myPrototype = {
-
-  swing() {
-    return this.swung = false;
-
-  },
-}
-
-
-
-// Ninja.prototype.swing = function() {
-//   // this.swung = true;
-//   return this.swung = true;
+// function Ninja() {
+//   this.swung = false;
 // }
 
-// Ninja.prototype = {
-//   swung: false,
-//   swing: function() {
-//     return this.swung = true;
-//   },
-
+// Ninja.prototype.swing = function() {
+//   this.swung = true;
+//   return this;
 // };
 
+// let ninjaA = new Ninja();
+// let ninjaB = new Ninja();
 
-// Add a swing method to the Ninja prototype which
-// modifies `swung` and returns the calling object
-
-let ninjaA = new Ninja();
-let ninjaB = new Ninja();
-
-console.log(ninjaA.swing().swung);      // logs `true`
-console.log(ninjaB.swing().swung);      // logs `true`
-
-
-
-
+// console.log(ninjaA.swing().swung);      // logs `true`
+// console.log(ninjaB.swing().swung);      // logs `true`
 
 
 
 
 
 // Problem 7
+// let ninjaA;
 
+// {
+//   const Ninja = function() {
+//     this.swung = false;
+//   };
 
+//   ninjaA = new Ninja();
+// }
 
+// let ninjaB = new ninjaA.constructor();
 
-
+// console.log(ninjaA.constructor === ninjaB.constructor); // => true
 
 
 
 
 
 // Problem 8
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function User(first, last) {
+  if (!(this instanceof User)) {
+    return new User(first, last);
+  }
+
+  this.name = first + ' ' + last;
+}
+
+let name = 'Jane Doe';
+let user1 = new User('John', 'Doe');
+let user2 = User('John', 'Doe');
+
+console.log(name);         // => Jane Doe
+console.log(user1.name);   // => John Doe
+console.log(user2.name);   // => John Doe
