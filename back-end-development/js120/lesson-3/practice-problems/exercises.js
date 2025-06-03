dbb3a8c9cba90519890b4b15522463f4d602b431
Austin Kim <austinkim61@gmail.com>
2025-06-03 17:56:30 -0400

    finished built-in constructors lesson

--- back-end-development/js120/lesson-3/practice-problems/exercises.js
+++ back-end-development/js120/lesson-3/practice-problems/exercises.js
@@ -41,53 +41,65 @@
 
 
 
-let DogPrototype = {
-  bark() {
-    console.log(this.weight > 20 ? 'Woof!' : 'Yip!');
+// let DogPrototype = {
+//   bark() {
+//     console.log(this.weight > 20 ? 'Woof!' : 'Yip!');
+//   }
+// };
+
+// function Dog(name, breed, weight) {
+//   Object.setPrototypeOf(this, DogPrototype);
+//   this.name = name;
+//   this.breed = breed;
+//   this.weight = weight;
+// }
+
+// Dog.prototype = DogPrototype;
+
+// let maxi = new Dog('Maxi', 'German Shepherd', 32);
+
+
+// // console.log(maxi.bark());
+// console.log(Dog.prototype.bark());
+
+
+
+
+
+
+
+
+// function Dog(name, breed, weight) {
+//   this.name = name;
+//   this.breed = breed;
+//   this.weight = weight;
+
+// }
+
+// Dog.allDogs = [];
+// Dog.species = 'Canis lupus';
+// Dog.kingdom = 'animal';
+
+
+
+
+
+
+
+
+let arrayFrom = obj => {
+  let len = obj.length;
+  let returnArr = [];
+
+  for (let idx = 0; idx < len; idx += 1) {
+    returnArr.push(obj[idx]);
   }
+
+  return returnArr;
 };
 
-function Dog(name, breed, weight) {
-  Object.setPrototypeOf(this, DogPrototype);
-  this.name = name;
-  this.breed = breed;
-  this.weight = weight;
-}
-
-Dog.prototype = DogPrototype;
-
-let maxi = new Dog('Maxi', 'German Shepherd', 32);
-
-
-// console.log(maxi.bark());
-console.log(Dog.prototype.bark());
-
-
-
-
-
-
-
-
-function Dog(name, breed, weight) {
-  this.name = name;
-  this.breed = breed;
-  this.weight = weight;
-
-}
-
-Dog.allDogs = [];
-Dog.species = 'Canis lupus';
-Dog.kingdom = 'animal';
-
-
-
-
-
-
-
-
-
+console.log(arrayFrom({0: 'a', 1: 'b', 2: 'c', length: 3}));
+// => [ 'a', 'b', 'c' ]
 
 
 
