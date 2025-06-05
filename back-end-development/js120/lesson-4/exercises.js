971180e0f67c01f59f70ad17e3fbc866bfec5b6e
Austin Kim <austinkim61@gmail.com>
2025-06-04 23:30:26 -0400

    saving progress

--- back-end-development/js120/lesson-4/exercises.js
+++ back-end-development/js120/lesson-4/exercises.js
@@ -1,34 +1,65 @@
+// class Human {
+//   myName() { return this.name; }
+//   myAge() { return this.age; }
+// }
+
+// class Person extends Human {
+//   toString() {
+//     return `My name is ${this.myName()} and I'm ${this.myAge()} years old.`;
+//   }
+// }
+
+// let will = new Person();
+// will.name = 'William';
+// will.age = 28;
+// console.log(will.toString()); // => My name is William and I'm 28 years old.
 
 
 
 
 
+function Greeting() {}
+
+Greeting.prototype.greet = function(message) {
+  console.log(message);
+};
+
+function Hello() {}
+
+Hello.prototype = Object.create(Greeting.prototype);
+
+Hello.prototype.hi = function() {
+  this.greet('Hello!');
+};
+
+function Goodbye() {}
+
+Goodbye.prototype = Object.create(Greeting.prototype);
+
+Goodbye.prototype.bye = function() {
+  this.greet("Goodbye");
+};
 
 
 
+// Case 1
+// let hello = new Hello();
+// hello.hi(); // 'Hello!'
 
+// Case 2
+// let hello = new Hello();
+// hello.bye(); // TypeError: hello.bye is not a function
 
+// Case 3
+// let hello = new Hello();
+// hello.greet(); // undefined
 
+// Case 4
+// let hello = new Hello();
+// hello.greet('Goodbye'); // 'Goodbye'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Case 5
+Hello.hi(); // Hello.hi is not a function
 
 
 
