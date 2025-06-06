75395f7bb0a9f279047c67ef9ca8af69113d4db8
Austin Kim <austinkim61@gmail.com>
2025-06-05 22:21:50 -0400

    finished subtyping with classes practice problem

--- back-end-development/js120/lesson-4/practice-problems/subtyping-with-classes.js
+++ back-end-development/js120/lesson-4/practice-problems/subtyping-with-classes.js
@@ -1,3 +1,24 @@
+// class Game {
+//   play() {
+//     return 'Start the game!';
+//   }
+// }
+
+// class Bingo extends Game {
+//   rulesOfPlay() {
+//     // rules of play
+//   }
+
+//   play() {
+//     return 'Bingo';
+//   }
+// }
+
+// let bingo = new Bingo();
+
+// console.log(bingo.play());
+
+// // it will call the method that is on Bingo since it is there. called method overriding
 
 
 
@@ -5,20 +26,30 @@
 
 
 
+class Greeting {
+  greet(string) {
+    console.log(string);
+  }
+};
+
+class Hello extends Greeting {
+  hi() {
+    this.greet('Hello');
+  }
+};
 
 
+class Goodbye extends Greeting {
+  bye() {
+    this.greet('Goodbye');
+  }
+};
 
+let firstHello = new Hello();
+let firstGoodbye = new Goodbye();
 
-
-
-
-
-
-
-
-
-
-
+firstHello.hi();
+firstGoodbye.bye();
 
 
 
