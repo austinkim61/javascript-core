7acc0369e93fa0d5e5eb8a520fdf6668bea38fba
Austin Kim <austinkim61@gmail.com>
2025-06-08 17:48:02 -0400

    finished js120 lesson 4

--- back-end-development/js120/lesson-4/practice-problems/exercises.js
+++ back-end-development/js120/lesson-4/practice-problems/exercises.js
@@ -189,78 +189,54 @@ constructor has a name property that contains the name of the class as a string
 
 
 // Problem 3
-const Moveable = {
-  range() {
-    return this.fuelCap *  this.fuelEfficiency;
-  }
-}
+// const Moveable = {
+//   range() {
+//     return this.fuelCap *  this.fuelEfficiency;
+//   }
+// }
 
-class WheeledVehicle {
-  constructor(tirePressure, kmTravelledPerLiter, fuelCapInLiter) {
-    this.tires = tirePressure;
-    this.fuelEfficiency = kmTravelledPerLiter;
-    this.fuelCap = fuelCapInLiter;
-  }
+// class WheeledVehicle {
+//   constructor(tirePressure, kmTravelledPerLiter, fuelCapInLiter) {
+//     this.tires = tirePressure;
+//     this.fuelEfficiency = kmTravelledPerLiter;
+//     this.fuelCap = fuelCapInLiter;
+//   }
 
-  tirePressure(tireIdx) {
-    return this.tires[tireIdx];
-  }
+//   tirePressure(tireIdx) {
+//     return this.tires[tireIdx];
+//   }
 
-  inflateTire(tireIdx, pressure) {
-    this.tires[tireIdx] = pressure;
-  }
-}
+//   inflateTire(tireIdx, pressure) {
+//     this.tires[tireIdx] = pressure;
+//   }
+// }
 
-Object.assign(WheeledVehicle.prototype, Moveable);
+// Object.assign(WheeledVehicle.prototype, Moveable);
 
-class Auto extends WheeledVehicle {
-  constructor() {
-    // the array represents tire pressure for four tires
-    super([30,30,32,32], 50, 25.0);
-  }
-}
+// class Auto extends WheeledVehicle {
+//   constructor() {
+//     // the array represents tire pressure for four tires
+//     super([30,30,32,32], 50, 25.0);
+//   }
+// }
 
-class Motorcycle extends WheeledVehicle {
-  constructor() {
-    // array represents tire pressure for two tires
-    super([20,20], 80, 8.0);
-  }
-}
+// class Motorcycle extends WheeledVehicle {
+//   constructor() {
+//     // array represents tire pressure for two tires
+//     super([20,20], 80, 8.0);
+//   }
+// }
 
-class Catamaran {
-  constructor(propellerCount, hullCount, kmTravelledPerLiter, fuelCapInLiter) {
-    this.propellerCount = propellerCount;
-    this.hullCount = hullCount;
-    this.fuelEfficiency = kmTravelledPerLiter;
-    this.fuelCap = fuelCapInLiter;
-  }
-}
+// class Catamaran {
+//   constructor(propellerCount, hullCount, kmTravelledPerLiter, fuelCapInLiter) {
+//     this.propellerCount = propellerCount;
+//     this.hullCount = hullCount;
+//     this.fuelEfficiency = kmTravelledPerLiter;
+//     this.fuelCap = fuelCapInLiter;
+//   }
+// }
 
-Object.assign(Catamaran.prototype, Moveable);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// Object.assign(Catamaran.prototype, Moveable);
 
 
 
