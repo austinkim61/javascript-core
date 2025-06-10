1349ccfa328bdcf62db1930e07acfa4f3afe23d2
Austin Kim <austinkim61@gmail.com>
2025-06-10 18:16:01 -0400

    starting oo ttt part 3

---
+++ back-end-development/js120/lesson-5/exercises.js
@@ -0,0 +1,29 @@
+
+
+
+class Board {
+  display() {
+    p(``);
+    p(`     |     |`);
+    p(`  O  |     |  O`);
+    p(`     |     |`);
+    p(`-----+-----+-----`);
+    p(`     |     |`);
+    p(`     |  X  |`);
+    p(`     |     |`);
+    p(`-----+-----+-----`);
+    p(`     |     |`);
+    p(`  X  |     |`);
+    p(`     |     |`);
+    p(``);
+  }
+  constructor() {
+    // STUB
+    // way to model the 3x3 grid.
+      // perhaps `squares`
+    // data structure to use
+      // array, object, something else
+    // what should the data structure store
+      // strings, numbers, square objects
+  }
+}
